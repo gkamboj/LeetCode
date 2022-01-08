9cad5f7897556a46ff049de37fd7f9a6b008dc72
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-09 00:30:16 +0530

    Update kadane-53-maximum-subarray.py

--- 53-maximum-subarray/kadane-53-maximum-subarray.py
+++ 53-maximum-subarray/kadane-53-maximum-subarray.py
@@ -2,10 +2,16 @@ class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         if len(nums) == 1: return nums[0]
         maxSum, currSum = -1000000, 0
-        for num in nums:
+        tempStart, start, end = 0, 0, 0
+        for ind, num in enumerate(nums):
             currSum += num
-            maxSum = max(maxSum, currSum)
-            if(currSum < 0): currSum = 0
+            if maxSum < currSum:
+                maxSum = currSum
+                end = ind
+                start = tempStart
+            if(currSum < 0):
+                currSum = 0
+                tempStart = ind + 1
         return maxSum
-#This approach is Kadane's algorithm
+#This approach is Kadane's algorithm and also gives start as well as end index of subarraya
 #See Java solution for divide and conquer

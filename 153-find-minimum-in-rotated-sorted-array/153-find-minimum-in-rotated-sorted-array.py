c453b7a44f64db428d8a6013274bec3504ca2b2f
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-11 00:54:09 +0530

    Time: 49 ms (29.64%), Space: 14.7 MB (30.07%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -1,20 +1,22 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
-        def find_rotated_index(nums,low,high,n):
-            if low <= high:
-                mid = (low + high) // 2
-                if mid + 1 <= n and nums[mid] > nums[mid + 1]:
-                    return mid + 1
-                else:
-                    if nums[mid] < nums[low]:
-                        return find_rotated_index(nums,low,mid-1,n)
-                    else:
-                        return find_rotated_index(nums,mid+1,high,n)
-                    
-            return -1
-        
-        rotated_index = find_rotated_index(nums,0,len(nums)-1,len(nums)-1)
-        if rotated_index == -1:
+        if len(nums) == 1 or nums[0] < nums[-1]:
             return nums[0]
-        else:
-            return min(nums[0],nums[rotated_index])
+
+        left, right = 0, len(nums) - 1
+        
+        while left <= right:
+            
+            mid = (left + right) // 2
+            print(mid)
+            if nums[mid - 1] > nums[mid]:  
+                return nums[mid]
+            
+            if nums[left] < nums[mid] < nums[right]:
+                right = mid - 1  
+                
+            elif nums[left] <= nums[mid] > nums[right]:
+                left = mid + 1
+                
+            elif nums[left] > nums[mid] < nums[right]:
+                right = mid - 1

7569ee72c297e27362de90cfe3dc3f42a9f654e4
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-12 00:56:27 +0530

    Time: 65 ms (15.23%), Space: 14.7 MB (30.13%) - LeetHub

--- 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
+++ 153-find-minimum-in-rotated-sorted-array/153-find-minimum-in-rotated-sorted-array.py
@@ -5,17 +5,14 @@ class Solution:
         if nums[0] < nums[-1]:
             return nums[0]
         start, end = 0, len(nums) - 1
-        while True:
+        while start < end:
             mid = (start + end) // 2
-            if nums[mid] < nums[mid - 1]:
-                return nums[mid]
-            elif nums[mid] > nums[mid + 1]:
-                return nums[mid + 1]
-            elif nums[mid] > nums[start]:
+            if nums[mid] > nums[end]:
                 start = mid + 1
             else:
-                end = mid - 1
-
+                end = mid
+        return nums[start]
+    
 #Approach: Binary search.
 
 #Check other submissions for more approaches.

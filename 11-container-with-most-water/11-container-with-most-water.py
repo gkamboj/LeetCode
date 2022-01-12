6578f70fea4254978934034079eee7e530fae3fd
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-13 01:49:37 +0530

    Time: 1395 ms (5.01%), Space: 27.1 MB (87.87%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -1,16 +1,15 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
         start, end = 0, len(height) - 1
-        currStart, currEnd = height[0], height[end]
-        ans = (len(height) - 1) * min(currStart, currEnd)
+        ans = (len(height) - 1) * min(height[0], height[-1])
         currArea = ans
         while start < end:
             if height[start] > height[end]:
                 end -= 1
             else:
                 start += 1
-            #if height[end] > currEnd or height[start] > currStart:
             currArea = (end - start) * min(height[start], height[end])
-                #currStart, currEnd = height[start], height[end]
             ans = max(ans, currArea)
         return ans
+
+#Approach: Use two pointers to start with area having max width. Go to a shorter width container only if there is a vertical line longer than the current containers shorter line. Keep in mind that for current container, lower height isn't supporting the higher water level and can thus be safely removed from further consideration.

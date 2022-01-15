c7ef2b5ffc53fd5daed8e94872da8f07f486197d
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-15 23:42:05 +0530

    Time: 266 ms (13.86%), Space: 16 MB (40.67%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -15,4 +15,8 @@ class Solution:
         if negs % 3 == 1:
             ans *= -1
         return ans
-        
+
+#Approach: For every 32 bits, count number of set bits. If it's not multiple of 3, then this bit is set for our answer. Find every set bit of answer.
+
+#Note-1: We are handling negative numbers separately because Python does not have int limit. Even if our answer value goes above (2^31 - 1), it does not change to negative. In Java, INT_MAX + 1 = INT_MIN
+#Note-2: See Java solution, which runs without handling of negative cases.

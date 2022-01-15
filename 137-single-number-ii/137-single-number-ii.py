85161a7c0d62a93de1160b698d799ae36e7a3ab7
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-15 23:14:03 +0530

    Time: 207 ms (16.26%), Space: 15.6 MB (94.87%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -1,18 +1,16 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans, i, negs = 0, 0, 0
-        for ind, num in enumerate(nums):
-            if num < 0:
-                nums[ind] = -1*num
-                negs += 1
+        ans, i = 0, 0
         while i < 32:
-            currBitsSet = 0
+            negs, currBitsSet = 0, 0
             for num in nums:
+                if num < 0:
+                    negs += 1
+                    num = abs(num)
                 if num & (1 << i) > 0:
                     currBitsSet += 1 
-                    currBitsSet %= 3
-            if currBitsSet == 1:
-                ans |= 1 << i
+            if currBitsSet % 3 == 1:
+                ans += 1 << i
             i += 1
         if negs % 3 == 1:
             ans *= -1

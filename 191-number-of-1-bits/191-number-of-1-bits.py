dffb0509cbd1e1f284750ccd8b0feda1bd8b0193
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-17 13:26:56 +0530

    Time: 38 ms (36.25%), Space: 14.1 MB (67.70%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.py
+++ 191-number-of-1-bits/191-number-of-1-bits.py
@@ -1,11 +1,11 @@
 class Solution:
     def hammingWeight(self, n: int) -> int:
         count = 0
-        while n > 0:
-            count += n & 1
-            n = n >> 1
+        while n != 0:
+            count += 1
+            n = n & (n - 1)
         return count
-    
-#Approach: Check for rightmost bit and increase count if it's set. Rightshift the n by 1 bit, Continue till n > 0. TC -> O(number of bits in N)
 
-#See other approach for a little optimised solution
+#Approach: n & (n - 1) unsets the rightmost set bet of n. So, keep unsetting rightmost bits till every bit unsets (and hence n becomes zero). TC -> O(no. of set bits)
+
+#See other submission of O(number of bits in N) approach

e81014b409db54cc468244419d946c51e90f6650
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-17 13:30:24 +0530

    Update set-bits-checked-number-of-1-bits.py

--- 191-number-of-1-bits/set-bits-checked-number-of-1-bits.py
+++ 191-number-of-1-bits/set-bits-checked-number-of-1-bits.py
@@ -8,4 +8,6 @@ class Solution:
 
 #Approach: n & (n - 1) unsets the rightmost set bet of n. So, keep unsetting rightmost bits till every bit unsets (and hence n becomes zero). TC -> O(no. of set bits)
 
+#See https://leetcode.com/problems/number-of-1-bits/discuss/55099/Simple-Java-Solution-Bit-Shifting to understand difference if working in Java
+
 #See other submission of O(number of bits in N) approach

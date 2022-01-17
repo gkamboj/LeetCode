f29d7952c4ea0237ac549fdd40cfb552d6f08401
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-17 23:01:02 +0530

    Time: 131 ms (16.02%), Space: 15.8 MB (66.74%) - LeetHub

--- 260-single-number-iii/260-single-number-iii.py
+++ 260-single-number-iii/260-single-number-iii.py
@@ -10,3 +10,7 @@ class Solution:
                 first ^= num
         second = singleAppXor ^ first
         return [first, second]
+    
+#Approach: Take XOR over the array. This will give the XOR of two numbers appearing once, say it to be singleAppXor. Find the rightmost set bit in singleAppXor, this bit will make sure one of the answer element has that bit as 0 and other as 1. Now, nums can be divided into two parts: one with that bit as set and other part having that bit as unset. Iterate over the one part of array (using condn in Line-9) and find one answer. Once we have an answer, other answer can be found out.
+
+#Note-1: Line-7 is giving us the number which is having only rightmost set bit, rest all 0s. 

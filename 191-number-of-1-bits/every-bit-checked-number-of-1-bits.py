2ec93aaa6531214d7519097f6e09914ba93c2436
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-17 13:30:44 +0530

    Update every-bit-checked-number-of-1-bits.py

--- 191-number-of-1-bits/every-bit-checked-number-of-1-bits.py
+++ 191-number-of-1-bits/every-bit-checked-number-of-1-bits.py
@@ -8,4 +8,6 @@ class Solution:
     
 #Approach: Check for rightmost bit and increase count if it's set. Rightshift the n by 1 bit, Continue till n > 0. TC -> O(number of bits in N)
 
+#See https://leetcode.com/problems/number-of-1-bits/discuss/55099/Simple-Java-Solution-Bit-Shifting to understand difference if working in Java
+
 #See other approach for a little optimised solution

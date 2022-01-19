c5d655d28d0de052db1f40f232f0fb4b7aaccf24
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-19 23:59:14 +0530

    Time: 763 ms (32.15%), Space: 15.6 MB (28.42%) - LeetHub

--- 477-total-hamming-distance/477-total-hamming-distance.py
+++ 477-total-hamming-distance/477-total-hamming-distance.py
@@ -8,3 +8,5 @@ class Solution:
                     onesCount += 1
             ans += onesCount * (len(nums) - onesCount)
         return ans
+    
+#Approach: Observe that sum of pairwise hamming distance is same as sum of x*y for every bit, where x = number of set bits and y = number of unset bits.

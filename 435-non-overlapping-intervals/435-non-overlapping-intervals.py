b07a889f9a0de638174218f9cb16d1442437ad2f
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-26 01:17:37 +0530

    Time: 1452 ms (65.33%), Space: 54.2 MB (5.17%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.py
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.py
@@ -4,11 +4,15 @@ class Solution:
             return 0
         intervals = sorted(intervals, key = lambda interval: interval[1])
         print(intervals)
-        high, ans = intervals[0][1], 0
+        currEnd, ans = intervals[0][1], 0
         for interval in intervals[1:]:
-            if interval[0] < high:
-                high = min(interval[1], high)
+            if interval[0] < currEnd:
+                currEnd = min(interval[1], currEnd)
                 ans += 1
             else:
-                high = interval[1]
+                currEnd = interval[1]
         return ans
+    
+#Approach: Sort the intervals wrt their start. For every overlap, remove the interval with higher end value (so as to avoid maximum overlapping) and increment answer. In case of no overlap, update currEnd variable to current end.
+
+#Note: For overlap, start should be less than end of other interval. There is no overlap on equality. For eg., there is no overlap for [1,2] & [2,3]

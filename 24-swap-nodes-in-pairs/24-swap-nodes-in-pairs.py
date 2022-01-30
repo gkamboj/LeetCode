ce1f9d691c2d4a019e16e917eacc1fbd963a2364
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-31 02:02:29 +0530

    Time: 46 ms (32.93%), Space: 13.9 MB (95.86%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.py
@@ -12,9 +12,11 @@ class Solution:
             nextNode = curr.next
             curr.next = nextNode.next
             nextNode.next = curr
-            prev.next = nextNode
-            prev = curr
+            prev.next = nextNode #Setting next of tail from previous iteration
+            prev = curr #Updating prev value for next iteration
             curr = curr.next
         return ans
         
-            
+#Approach: Save 2nd node initially as that will be the final head node. Now, swap every pair and keep saving the first value so as to use it as prev in next iteration.
+
+#Explore for iterative approach.

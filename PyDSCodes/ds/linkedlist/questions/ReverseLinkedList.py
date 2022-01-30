adf1807e3facc6d0aca668856e31393708ca2a8d
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-01-30 14:32:36 +0530

    Added recursive methods to reverse a LinkedList

--- PyDSCodes/ds/linkedlist/questions/ReverseLinkedList.py
+++ PyDSCodes/ds/linkedlist/questions/ReverseLinkedList.py
@@ -12,6 +12,23 @@ def reverseLinkedListIteratively(linkedList):
     linkedList.head = prev
 
 
+def reverseLinkedListRecursively(head):
+    if not head or not head.next:
+        return head
+    rest = reverseLinkedListRecursively(head.next)
+    head.next.next = head
+    head.next = None
+    return rest
+
+
+def reverseLinkedListRecursively2(head, prev):
+    if not head:
+        return prev
+    newHead = head.next
+    head.next = prev
+    return reverseLinkedListRecursively2(newHead, head)
+
+
 def helper(nodesDataList):
     linkedList = LinkedList.createLinkedList(nodesDataList)
     print("Original linked list: ", end=" ")
@@ -19,6 +36,13 @@ def helper(nodesDataList):
     reverseLinkedListIteratively(linkedList)
     print("Reversed linked list through iteration: ", end=" ")
     linkedList.printList()
-
+    linkedList.head = reverseLinkedListRecursively(linkedList.head)
+    print("Reversed linked list through recursion: ", end=" ")
+    linkedList.printList()
+    linkedList.head = reverseLinkedListRecursively2(linkedList.head, None)
+    print("Reversed linked list through recursion (way 2): ", end=" ")
+    linkedList.printList()
+    print()
 
 helper([1, 2, 3, 4, 5])
+helper([1])

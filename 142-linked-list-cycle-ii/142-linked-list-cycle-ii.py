d54b3a4d5c98a9e04c2a0e44d4c6aa1259cce5f1
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-02-11 00:26:55 +0530

    Time: 60 ms (62.98%), Space: 17.3 MB (30.15%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.py
@@ -17,10 +17,16 @@ class Solution:
                 break
         if not isLoop:
             return None
-        slow = head
+        fast = head
         while slow != fast:
             print(slow.val, fast.val)
             slow = slow.next
             fast = fast.next
         return slow
             
+#Approach:
+#A. To check if loop exists: Take start and fast pointers. If they both ever meet after starting from head, then there is a loop. This works because:
+#   Say, loop size is c and currently slow is at loop entry point & fast is at some point k from entry point i.e. distance between both pointers is k. From this point, every iterative movement of slow and fast will inrease their distance by 1. Both pointers will continue to move till distance increases from k to c. Once this distance is k, it's equivalent to zero distance (because we are in loop) and hence there is a meeting between both pointers. This proves both pointers will always meet if there is a loop.
+
+#B. To find the entry/beginning point of loop, move one pointer from the collision point (found above) to head and keep moving both pointers (from head and collision point) by a single distance They will meet at entry point. This works because:
+#   Refer Beginning_point_of_cycle_in_linked_list.pdf

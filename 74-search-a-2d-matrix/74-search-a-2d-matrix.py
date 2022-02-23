1b5482db80b742a5dd752651540fb4bacc58f2da
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-02-23 23:49:02 +0530

    Time: 32 ms (99.77%), Space: 14.4 MB (77.29%) - LeetHub

--- 74-search-a-2d-matrix/74-search-a-2d-matrix.py
+++ 74-search-a-2d-matrix/74-search-a-2d-matrix.py
@@ -16,4 +16,8 @@ class Solution:
                 high = mid - 1
         return matrix[low // cols][low % cols] == target
     
-#Approach: This is binary search approach. Treat 2-D array as 1-D and apply binary search approach. Remember few conditions: use low < high (not low != high -> this will be issue if high becomes lesser than low).
+#Approach: This is binary search approach. Treat 2-D array as 1-D and apply binary search approach. Remember few conditions:
+#1. use low < high (not low != high -> this will be issue if high becomes lesser than low)
+#2. While returning, check if current element is same as target to handle cases like input -> [[1]], 1
+
+#See other submissions for alternate approaches

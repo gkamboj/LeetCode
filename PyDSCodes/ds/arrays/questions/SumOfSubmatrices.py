6bd3a71c9dc2a54b063666f63343726dcc59b3c4
Gaurav Kamboj <Gaurav1.Kamboj@ril.com>
2022-04-01 13:20:05 +0530

    Sum of all submatrices: added approach comment

--- PyDSCodes/ds/arrays/questions/SumOfSubmatrices.py
+++ PyDSCodes/ds/arrays/questions/SumOfSubmatrices.py
@@ -18,3 +18,9 @@ matrix2 = [[1, 2, 3],
            [7, 8, 9]]
 
 print(findAllSubmatricesSum(matrix))
+
+# Approach: Multiply each element with the x where x is the number of sub-matrices that element will appear in. To
+# find x, observe that mat[i][j] will be part of all sub-matrices which start from any point till [i, j] (including i
+# and j) and end after [i, j] (again including i and j). Number of ways if finding start point = (i + 1) * (j + 1)
+# because i and j are 0-based indices => number of values till i = (i + 1). Similarly, for end: (m + 1 - (i + 1))
+# * (n + 1 - (j + 1_)) = (m - i) * (n - j). So, answer = (i + 1) * (j + 1) * (m - i) * (n - j)

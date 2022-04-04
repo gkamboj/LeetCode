3008931093c2a31617bca566640e961f8c4e8db2
Gaurav Kamboj <Gaurav1.Kamboj@ril.com>
2022-04-04 22:22:58 +0530

    Wave Array

--- PyDSCodes/ds/arrays/questions/WaveArray.py
+++ PyDSCodes/ds/arrays/questions/WaveArray.py
@@ -1,19 +1,17 @@
 # Given an array of integers A, sort the array into a wave like array and return it, In other words, arrange the
 # elements into a sequence such that
-# a1 >= a2 <= a3 >= a4 <= a5…..
+# a1 >= a2 <= a3 >= a4 <= a5….
 # Note: If there are multiple answers possible, return the one that's lexicographically smallest.
 
 def waveArray(arr):
     arr.sort()
-    if len(arr) % 2 == 0:
-        n = len(arr)
-    else:
-        n = len(arr) - 1
-    for ind in range(0, n, 2):
+    for ind in range(0, (len(arr) // 2) * 2, 2):
         arr[ind], arr[ind + 1] = arr[ind + 1], arr[ind]
     return arr
 
 
 arr = [5, 9, 3, 8, 11, 1, 4, 13, 12]
-arr2 = [7, 11, 6, -66, 23, 56, 99, -12, 0]
+arr2 = [7, 11, 6, -66, 23, 56, 99, -12, 0, 10]
 print(waveArray(arr2))
+
+# Approach: Sort the array and swap adjacent elements in pairs. Remember to handle even-odd length array cases.

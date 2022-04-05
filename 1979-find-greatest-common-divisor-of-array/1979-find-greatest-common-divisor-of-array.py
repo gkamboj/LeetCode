fb5c4a4a377f3e69d6b3e58fb9d824723b47fa31
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-05 10:49:32 +0530

    Time: 60 ms (85.80%), Space: 14.2 MB (9.00%) - LeetHub

--- 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.py
@@ -5,9 +5,18 @@ class Solution:
             minVal = min(minVal, num)
             maxVal = max(maxVal, num)
         
-        while minVal > 0:
-            rem = maxVal % minVal
-            maxVal = minVal
-            minVal = rem
-        
-        return maxVal
+        return self.findGCDRecursive(minVal, maxVal)
+    
+    def findGCDRecursive(self, num1, num2):
+        if num2 == 0:
+            return num1
+        return self.findGCDRecursive(num2, num1 % num2)
+    
+    def findGCDIterative(self, num1, num2):
+        while num2 > 0:
+            rem = num1 % num2
+            num1 = num2
+            num2 = rem
+        return num1
+    
+#Approach: Euclid Algorithm -> GCD(a, b) = GCD(b, a % b) = GCD(a % b, b). This has been implemented through both iterative and recursive ways.

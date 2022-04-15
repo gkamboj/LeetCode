2efef5cbe003ce1cb1e7fb5254ecd1e8055f2d62
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-15 12:17:45 +0530

    Time: 28 ms (97.03%), Space: 13.7 MB (96.28%) - LeetHub

--- 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
+++ 172-factorial-trailing-zeroes/172-factorial-trailing-zeroes.py
@@ -5,3 +5,5 @@ class Solution:
             count += n // 5
             n //= 5
         return count
+    
+#Approach: Every 0 in factorial is contributed by factors 5 and 2. Also, in a factorial factorisation, powerof 5 < power of 2, so just count the power of 5 to get the answer.

509c8db82aa8cd6510144221dd5cdeb16bd24b4e
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-16 20:13:02 +0530

    Time: 24 ms (99.00%), Space: 13.9 MB (77.49%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -7,3 +7,5 @@ class Solution:
             ans *= i
             ans //= (i - max(m, n) + 1)
         return int(ans)
+    
+# Approach: Problem is to find number of ways of taking (n - 1) horizontal or (m - 1) vertical steps out of (m + n - 2) total steps that is (m + n - 2)C(m - 1). As m and n can be really large, directly applying factorial formula may result in values out of integer range. So, solve nCr = [(n - r + 1).(n - r + 2)....(n - 2).(n - 1).n] / [1.2.3....(r - 1).r]

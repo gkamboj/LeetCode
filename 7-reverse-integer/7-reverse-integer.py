03af3b5c29912a00ae9414ade9eb7a6ab146f3d3
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-18 23:33:01 +0530

    Time: 40 ms (66.64%), Space: 13.8 MB (67.04%) - LeetHub

--- 7-reverse-integer/7-reverse-integer.py
+++ 7-reverse-integer/7-reverse-integer.py
@@ -3,11 +3,12 @@ class Solution:
         ans = 0
         absx = abs(x)
         while absx > 0:
-            rem = absx % 10
-            ans = ans * 10 + rem
+            ans = ans * 10 + absx % 10
             absx //= 10
         if ans > 2 ** 31 - 1:
             return 0
         if x < 0:
             ans *= -1
         return ans
+    
+#Approach: Reversing part is self-explanatory. For overflow part, comparison with (2 ** 31 - 1) has been done because Python does not have overflow concept. But this approach will fail in Java. For Java, check after every iteration if current value is overflown by doing similar to https://leetcode.com/problems/reverse-integer/discuss/4060/My-accepted-15-lines-of-code-for-Java 

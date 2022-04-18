28c802972495f0074abcd3b7094a7a6a499a8f5a
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-18 22:18:36 +0530

    Time: 22 ms (98.81%), Space: 13.8 MB (96.78%) - LeetHub

--- 168-excel-sheet-column-title/168-excel-sheet-column-title.py
+++ 168-excel-sheet-column-title/168-excel-sheet-column-title.py
@@ -7,3 +7,4 @@ class Solution:
             columnNumber //= 26
         return ans
         
+#Approach: This is equivalent to decimal to binary conversion with base 26. Note that unlike base 10 where we have decimals from 0 to 9, here we have 1 (A) to 26 (Z). To make the conversion simpler, we can change this range to 0 to 25 by reducing A by 1 in every iteration.

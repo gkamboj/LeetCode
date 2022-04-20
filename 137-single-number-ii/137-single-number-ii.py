45e9a9c4e048cd5584068fe222b4103b9f4b3417
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-20 23:54:35 +0530

    Time: 266 ms (13.86%), Space: 16 MB (40.67%)

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -1,19 +1,16 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        ans, i = 0, 0
-        while i < 32:
-            negs, currBitsSet = 0, 0
+        ans, negCount = 0, 0
+        for i in range(32):
+            setBits = 0
             for num in nums:
                 if num < 0:
-                    negs += 1
                     num = abs(num)
-                if num & (1 << i) == (1 << i):
-                    currBitsSet += 1 
-            if currBitsSet % 3 == 1:
-                ans += 1 << i
-            i += 1
-        if negs % 3 == 1:
-            ans *= -1
+                    negCount += 1
+                setBits += (num >> i) & 1
+            ans |= (setBits % 3) << i
+        if negCount % 3 != 0:
+            ans = -1 * ans
         return ans
 
 #Approach: For every 32 bits, count number of set bits. If it's not multiple of 3, then this bit is set for our answer. Find every set bit of answer.

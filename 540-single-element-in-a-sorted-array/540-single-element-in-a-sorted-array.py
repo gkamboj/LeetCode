cb454dc60137495032147e321ab592c2adbe2653
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-21 23:02:01 +0530

    Time: 180 ms (85.80%), Space: 23.8 MB (13.22%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -3,7 +3,9 @@ class Solution:
         low, high = 0, len(nums) - 1
         while low < high:
             mid = (low + high) // 2
-            if (mid % 2 == 1 and nums[mid] == nums[mid - 1]) or (mid % 2 == 0 and nums[mid] == nums[mid + 1]):
+            if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]:
+                return nums[mid]
+            elif (mid % 2 == 1 and nums[mid] == nums[mid - 1]) or (mid % 2 == 0 and nums[mid] == nums[mid + 1]):
                 low = mid + 1
             else:
                 high = mid

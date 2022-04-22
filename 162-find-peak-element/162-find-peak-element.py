5c5e573d82f40420a4d100699b9c520f0c426b1f
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-22 22:00:19 +0530

    Time: 54 ms (68.05%), Space: 14 MB (84.99%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -12,3 +12,6 @@ class Solution:
         return low
     
 #Approach: Since we need to find just one peak element (and not all), we can apply binary search. This works because if nums[mid] > nums[mid + 1], that means peak has to be on left: either whole array will be decreasing (nums[0] will be peak) or there will be dip somewhere on left side. Similarly when nums[mid] < nums[mid + 1], peak will be on right.
+
+#Note: low <= high, high = (mid - 1) will not work.
+#Note: Use conditions low <= high and high = (mid - 1) only when we are not comparing with adjacent elements, else it'll cause index out of bound exception

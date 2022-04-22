45386cccc306fb960e6a7f6b0411209e8c32de34
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-22 22:07:20 +0530

    Time: 215 ms (57.09%), Space: 23.8 MB (47.29%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.py
@@ -3,12 +3,14 @@ class Solution:
         low, high = 0, len(nums) - 1
         while low < high:
             mid = (low + high) // 2
+           # print(mid)
             if nums[mid] != nums[mid - 1] and nums[mid] != nums[mid + 1]:
                 return nums[mid]
-            elif (mid % 2 == 1 and nums[mid] == nums[mid - 1]) or (mid % 2 == 0 and nums[mid] == nums[mid + 1]):
-                low = mid + 1
-            else:
+            elif (mid % 2 == 1 and nums[mid] == nums[mid + 1]) or (mid % 2 == 0 and nums[mid] == nums[mid - 1]):
+                
                 high = mid
+            else:
+                low = mid + 1
         return nums[low]
     
 # Approach: As the array is sorted, binary search can be applied. For deciding condition, observe that every poir will start at even index till the single element is not encountered. Once the single element is encountered, every pair will start at odd index. From this we can conclude when to search towards right (and hence making low = mid + 1).

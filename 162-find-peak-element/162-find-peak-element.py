346d2fc7b9e8f4c7710ef71dc92085b42b9c12a7
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-22 21:46:50 +0530

    Time: 59 ms (57.39%), Space: 13.9 MB (84.99%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -5,10 +5,10 @@ class Solution:
             mid = (low + high) // 2
             if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
                 return mid
-            elif nums[mid] > nums[mid + 1]:
-                high = mid
-            else:
+            elif nums[mid] < nums[mid + 1]:
                 low = mid + 1
+            else:
+                high = mid
         return low
     
 #Approach: Since we need to find just one peak element (and not all), we can apply binary search. This works because if nums[mid] > nums[mid + 1], that means peak has to be on left: either whole array will be decreasing (nums[0] will be peak) or there will be dip somewhere on left side. Similarly when nums[mid] < nums[mid + 1], peak will be on right.

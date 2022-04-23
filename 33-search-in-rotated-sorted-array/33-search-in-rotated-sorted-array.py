8fadbf57fb730535ab373f168611ce29c5164e80
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-23 22:08:59 +0530

    Time: 84 ms (5.34%), Space: 14.3 MB (99.92%)

--- 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.py
@@ -1,25 +1,26 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        if len(nums) == 1:
-            return 0 if nums[0] == target else -1
-        start, end = 0, len(nums) - 1
-        while start <= end:
-            mid = (start + end) // 2
+        low, high = 0, len(nums) - 1
+        ans = -1
+        while low <= high:
+            mid = (high - low) // 2 + low
             if nums[mid] == target:
-                return mid
-            elif nums[mid] < nums[end]:
-                if nums[mid] < target and nums[end] >= target:
-                    start = mid + 1
+                ans = mid
+                break
+            elif nums[mid] > nums[high]:
+                if nums[mid] > target >= nums[low]:
+                    high = mid - 1
                 else:
-                    end = mid - 1
+                    low = mid + 1
             else:
-                if nums[mid] > target and nums[start] <= target:
-                    end = mid - 1
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
                 else:
-                    start = mid + 1
-        return -1
+                    high = mid - 1
+        return ans
     
-#Approach:
-# Binary search with iterations.
-# Note-1: Focus on where to use = (i.e. <= and >=) in lines 11 and 16 
-# Note-2: changing line-7 condition to start < end will require another check of comparing target with nums[start] after the end of while loop
+# Approach: Binary search approach - find mid and use value at mid to check if pivot is on left or right:
+# 1. If pivot is on right: if target lies between nums[start] and nums[mid] that is if target lies in left part which is monotonically increasing half of array then set high to (mid - 1). If not, that means target lies in pivot half => so set low to (mid + 1)
+# 2. If pivot is on half: if target lies between nums[mid] and nums[end] that is if target lies in right part which is monotonically increasing half of array then set low to (mid + 1). If not, that means target lies in pivot half => set high to (mid - 1)
+
+# Note: Since there is no comparison with adjacent elements in main method, minMat <= maxMat is used (and not minMat < maxMat).

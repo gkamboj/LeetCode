b258edc0890ea90de4d861380124a8bb377650b6
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-26 00:44:23 +0530

    Time: 68 ms (25.12%), Space: 14 MB (60.05%)

--- 46-permutations/recusrive-permutations.py
+++ 46-permutations/recusrive-permutations.py
@@ -1,9 +1,9 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = []
-        
         if len(nums) == 1:
             return [nums[:]]
+        
+        result = []
         
         for ind in range(len(nums)):
             num = nums.pop(0)

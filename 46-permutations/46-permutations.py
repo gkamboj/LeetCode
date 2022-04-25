55c4d09cb9636cccb95e6502aa1b6bc1127e6d2c
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-26 00:43:35 +0530

    Time: 50 ms (61.44%), Space: 13.9 MB (85.42%) - LeetHub

---
+++ 46-permutations/46-permutations.py
@@ -0,0 +1,20 @@
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        #result = []
+        
+        if len(nums) == 1:
+            return [nums[:]]
+        
+        result = []
+        
+        for ind in range(len(nums)):
+            num = nums.pop(0)
+            perms = self.permute(nums)
+            for perm in perms:
+                perm.append(num)
+            nums.append(num)
+            result += perms
+        
+        return result
+    
+# Approach: This is recursive approach. If length of input is 1, then return the list of shallow copy of input (list of list is created as it'll be used afterwards to add other elements). Now, for num in nums (by popping the num from nums), find the permutations recursively and then add it back to the list at end. Also, update the result list.

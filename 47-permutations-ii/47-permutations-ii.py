f22ce13782c6effb9ebf0ae83da82db0e420ab1f
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-26 22:09:08 +0530

    Time: 102 ms (41.78%), Space: 14.2 MB (61.30%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -2,19 +2,23 @@ from collections import defaultdict
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
+        nums.sort()
+        return self.recursivePermuteUnique(nums)
+    
+    def recursivePermuteUnique(self, nums):
         if len(nums) == 1:
             return [[nums[0]]]
         
-        result, covered = [], defaultdict(int)
+        result, prev = [], -11
         
         for ind in range(len(nums)):
             num = nums.pop(0)
-            if covered[num] != 1:
-                perms = self.permuteUnique(nums)
+            if num != prev:
+                perms = self.recursivePermuteUnique(nums)
                 for perm in perms:
                     perm.append(num)
-                covered[num] = 1
                 result += perms
+            prev = num
             nums.append(num)
             
         return result

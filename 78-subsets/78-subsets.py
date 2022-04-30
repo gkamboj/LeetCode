2b8752361567bbaf42a370c83a890979b95e64f2
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-30 19:27:58 +0530

    Time: 45 ms (56.07%), Space: 14.2 MB (35.00%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -5,7 +5,6 @@ class Solution:
         
         result = []
         
-        #while len(nums) > 0:
         num = nums.pop(-1)
         sets = self.subsets(nums)
         result += [subset[:] for subset in sets]
@@ -15,6 +14,6 @@ class Solution:
     
 # Approach: This is recursive approach. Find the subsets of remaining array after popping out an element num. Now, append these subsets to result and then for every subset of updated result, append another subset by including num. For baae condition, return [[], [num[0]]] when len(nums) == 1 (because for set of size 1, subsets are empty set and the given set)
 
-# Note-1: Observe that insetad of for loop, while condition is used in line-8. This is because num is not appended back after popping (which was being done for permutations) and hence length of nums is decreasing.
+# Note-1: Observe that unlike permutations problem, no for loop is used here. This is because we need to consider any element only once and not add it back after consideration.
 
-# Note-2:
+# Note-2: Result will not be in sorted or lexicographic order.

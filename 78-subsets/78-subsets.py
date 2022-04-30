2c3b6d9bf2a6b7751ed65b355ce68c3f82dd8d89
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-04-30 19:16:04 +0530

    Time: 34 ms (88.06%), Space: 14 MB (95.96%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -6,3 +6,12 @@ class Solution:
             result += [subset + [num] for subset in result]
             
         return result
+    
+# Approach: This is iterative approach. Initialise the result variable with array of size one with empty list being the only element. Now, for every element in nums, append num to every element of result.
+# Illustration: Input: [1, 2, 3], result = [[]]
+# num = 1 => result = [[]] + [[1]] = [[], [1]]
+# num = 2 => result = [[], [1]] + [[2], [1, 2]] = [[], [1], [2], [1, 2]]
+# num = 3 => result = [[], [1], [2], [1, 2]] + [[3], [1, 3], [2, 3], [1, 2, 3]] = [[], [1], [2], [1, 2], [3], [1, 3], [2, 3], [1, 2, 3]]
+
+# Note-1: This is the first approach of LC official solution (i.e. Cascading)
+# Note-2: Result will not be in sorted or lexicographic order

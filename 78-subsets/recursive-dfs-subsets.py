e63a8d33f407906883f30900035e32fe889f5cfe
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-01 21:49:08 +0530

    Time: 56 ms (29.16%), Space: 14.1 MB (34.95%)

--- 78-subsets/recursive-dfs-subsets.py
+++ 78-subsets/recursive-dfs-subsets.py
@@ -11,7 +11,8 @@ class Solution:
     
 # Approach: This is recursive approach using DFS. Start with first element of array and find all subsets starting with that element. After that repeat the process for subsequent elements. Since there are 2^n subsets which we need to find, TC: O(2^n)
 
-# Reference: https://leetcode.com/problems/subsets-ii/discuss/30305/Simple-python-solution-(DFS)
+# Reference-1: https://leetcode.com/problems/subsets/discuss/27301/Python-easy-to-understand-solutions-(DFS-recursively-Bit-Manipulation-Iteratively)
+# Reference-2: https://leetcode.com/problems/subsets-ii/discuss/30305/Simple-python-solution-(DFS)
 
 # Dry Run:
 # subsetsRecursively([1, 2, 3], [], [])
@@ -24,4 +25,4 @@ class Solution:
 # i = 2 => subsetsRecursively([], [3], [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3]])
 # => result = [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
 
-# Note: This approach returns solution is sorted as well as lexicographic order.
+# Note: This approach returns solution is sorted as well as lexicographic order if initial input passed to recursive method is sorted.

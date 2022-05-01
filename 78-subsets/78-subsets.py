07e82b0562a3a19cf8a49e446b4b09a1a9a1271c
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-01 21:41:55 +0530

    Time: 52 ms (37.87%), Space: 14.1 MB (81.68%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,24 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        self.subsetsRecursively(nums, [], result)
+        return result
+    
+    def subsetsRecursively(self, nums, subset, result):
+        result.append(subset)
+        for i in range(len(nums)):
+            self.subsetsRecursively(nums[i + 1:], subset + [nums[i]], result)
+    
+# Approach: This is recursive approach using DFS. Start with first element of array and find all subsets starting with that element. After that repeat the process for subsequent elements. Since there are 2^n subsets which we need to find, TC: O(2^n)
+# Dry Run:
+# subsetsRecursively([1, 2, 3], [], [])
+# i = 0 => subsetsRecursively([2, 3], [1], [[]])
+#       i' = 0 => subsetsRecursively([3], [1, 2], [[], [1]])
+#              i'' = 0 =>  subsetsRecursively([], [1, 2, 3], [[], [1], [1, 2]])
+#       i' = 1 => subsetsRecursively([], [1, 3], [[], [1], [1, 2], [1, 2, 3]]) //in current level, subset was [1] => 2nd parameter = [1] + [3] = [1, 3]
+# i = 1 => subsetsRecursively([3], [2], [[], [1], [1, 2], [1, 2, 3], [1, 3]])
+#       i' = 0 => subsetsRecursively([], [2, 3], [[], [1], [1, 2], [1, 2, 3], [1, 3], [2]])
+# i = 2 => subsetsRecursively([], [3], [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3]])
+# => result = [[], [1], [1, 2], [1, 2, 3], [1, 3], [2], [2, 3], [3]]
+
+# Note: This approach returns solution is sorted as well as lexicographic order.

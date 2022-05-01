d62557b15042779f284feefa7eb9bad670282d20
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-01 22:49:15 +0530

    Time: 74 ms (6.48%), Space: 14.1 MB (81.68%) - LeetHub

---
+++ 78-subsets/78-subsets.py
@@ -0,0 +1,15 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        result = []
+        for i in range(1 << len(nums)):
+            subset = []
+            for j in range(len(nums)):
+                if (i >> j) & 1 == 1:
+                    subset.append(nums[j])
+            result.append(subset)
+            
+        return result
+    
+# Approach: This is bitwise approach. We know that for array of size n, 2^n subsets exists. So for every number from 0 to (2^n - 1), consider the set bits and form subset of elements present at those set bits. As every number has unique binary representation, we'll get unique subsets at the end.
+
+# Note: This approach will give sorted and lexicographic result if (sorted if input array is sorted)

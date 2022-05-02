db99303cc92299b4f74961114ddc38eb6cdf8e3d
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-02 11:13:01 +0530

    Time: 107 ms (54.59%), Space: 14 MB (26.28%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -1,18 +1,41 @@
 class Solution:
-    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        result = []
-        self.combinationSumRecursive(0, candidates, target, [], result)
-        return result
-    
-    def combinationSumRecursive(self, ind, arr, target, combination, result):
-        if target == 0:
-            result.append(combination)
+     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        n = len(candidates)
+        
+        candidates.sort()        
+        if n<=0 or candidates[0] <= 0:
+            return []
+        
+        subSets, tempList, posList = [], [], []
+        currSum = pos = 0
+        
+        while pos < n:
+            if currSum > target:
+                # pop curr and it's parent, because we are sorted and distinct
+                # adding any more items will only increase the sum
+                t = 2
+                while tempList and t:
+                    currSum -= tempList.pop()
+                    pos = posList.pop()
+                    t -= 1
+                pos += 1
+            elif currSum == target:
+                # Found a subset
+                subSets.append(tempList[:])
+                currSum -= tempList.pop()
+                pos = posList.pop()
+                pos += 1
+            else:
+                currSum += candidates[pos]
+                tempList.append(candidates[pos])
+                posList.append(pos)
             
-        for i in range(ind, len(arr)):
-            if arr[i] > target:
-                continue
-            self.combinationSumRecursive(ind, arr[i:], target - arr[i], combination + [arr[i]], result)
+            # At the end of each level, check if we still have some levels left to check
+            while pos >= n and posList:
+                # print("inside",tempList ,posList, currSum)
+                currSum -= tempList.pop()
+                pos = posList.pop() + 1
+                
+            # print(pos, tempList ,posList, currSum)
             
-# Approach: This is recursive approach using DFS similar to DFS submissions of LC-78 & LC-90. To further reduce the time (in comparison to reference), check is added to ignore the iteration if element is greater than target (as combination is not possible in such case). Instead of continue, break should be used if input array is sorted.
-
-# Reference: https://leetcode.com/problems/combination-sum/discuss/16510/Python-dfs-solution.
+        return subSets

d2cf9f84f4ebe69d5d7c14eaa6def701b6fe7d9d
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-02 23:50:57 +0530

    Time: 78 ms (83.18%), Space: 14 MB (59.29%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -24,3 +24,18 @@ class Solution:
                 currSum -= candidates[currPos - 1]
             
         return result
+    
+# Approach: This is iterative approach. Following is the dry run for input [3, 7, 2, 6]:
+#   currSum           tempList            posList             pos
+#       2               [2]                 [0]                 0
+#       4              [2, 2]              [0, 0]               0
+#       6             [2, 2, 2]           [0, 0, 0]             0
+#       8            [2, 2, 2, 2]        [0, 0, 0, 0]           0
+#       4               [2, 2]             [0, 0]               1
+#       7             [2, 2, 3]           [0, 0, 1]             1
+#       4               [2, 2]              [0, 0]              2
+#       11            [2, 2, 6]            [0, 0, 2]            2
+#       2                [2]                 [0]                3
+#       0                 []                  []                1
+
+# Reference: https://leetcode.com/problems/combination-sum/discuss/391125/Explanation-for-backtracking-solution-using-iteration-python-implementation-beats-90

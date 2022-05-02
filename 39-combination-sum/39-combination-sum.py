85aad5bda5f89b633e419eace6caa93c57c7d91b
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-02 10:22:55 +0530

    Time: 119 ms (44.08%), Space: 13.9 MB (91.94%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        self.combinationSumRecursive(candidates, target, [], result)
+        return result
+    
+    def combinationSumRecursive(self, arr, target, combination, result):
+        if target == 0:
+            result.append(combination)
+        
+        if target < 0:
+            return
+            
+        for i in range(len(arr)):
+            self.combinationSumRecursive(arr[i:], target - arr[i], combination + [arr[i]], result)
+            

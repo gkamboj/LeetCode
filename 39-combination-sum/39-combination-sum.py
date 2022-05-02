a6f3a4493bcc76d048ca9c84755b453d20c74e11
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-02 10:44:36 +0530

    Time: 92 ms (70.25%), Space: 14 MB (26.28%) - LeetHub

--- 39-combination-sum/39-combination-sum.py
+++ 39-combination-sum/39-combination-sum.py
@@ -13,3 +13,6 @@ class Solution:
                 continue
             self.combinationSumRecursive(arr[i:], target - arr[i], combination + [arr[i]], result)
             
+# Approach: This is recursive approach using DFS similar to DFS submissions of LC-78 & LC-90. To further reduce the time (in comparison to reference), check is added to ignore the iteration if element is greater than target (as combination is not possible in such case). Instead of continue, break should be used if input array is sorted.
+
+# Reference: https://leetcode.com/problems/combination-sum/discuss/16510/Python-dfs-solution.

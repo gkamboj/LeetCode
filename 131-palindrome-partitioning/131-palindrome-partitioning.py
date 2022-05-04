bcd305d8871076e3c16bc0307e1c5cffac0d6f81
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-04 23:39:26 +0530

    Time: 1222 ms (8.74%), Space: 29 MB (79.81%) - LeetHub

---
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -0,0 +1,20 @@
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
+        result = []
+        self.palindromePartitioning(s, [], result)
+        return result
+        
+    def palindromePartitioning(self, s, curr, result):
+        if not s:
+            result.append(curr)
+            return
+        
+        for i in range(len(s)):
+            if self.isPalindrome(s[:i+1]):
+                self.palindromePartitioning(s[i+1:], curr + [s[:i+1]], result)
+            
+    def isPalindrome(self, s):
+        for i in range(len(s) // 2):
+            if s[i] != s[-i-1]:
+                return False
+        return True

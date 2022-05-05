e5b5076d322cd705383e3f67efd9986cb8add934
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-06 00:00:16 +0530

    Time: 174 ms (38.25%), Space: 21.4 MB (69.52%) - LeetHub

--- 89-gray-code/89-gray-code.py
+++ 89-gray-code/89-gray-code.py
@@ -2,6 +2,8 @@ class Solution:
     def grayCode(self, n: int) -> List[int]:
         start, result = 1, [0]
         while start <= n:
-            result += [i + 2 ** (start - 1) for i in reversed(result)]
+            result += [i + (1 << (start - 1)) for i in reversed(result)]
             start += 1
         return result
+    
+# Approach: This is an iterative approach.

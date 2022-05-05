59e5e6ead209fddc37748d183ca450e234e1bf6b
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-06 00:48:13 +0530

    Time: 131 ms (70.02%), Space: 21.4 MB (41.93%) - LeetHub

--- 89-gray-code/89-gray-code.py
+++ 89-gray-code/89-gray-code.py
@@ -6,4 +6,12 @@ class Solution:
             start += 1
         return result
     
-# Approach: This is an iterative approach.
+# Approach: This is an iterative approach. Use the fact that for every 2^n gray codes, next 2^n gray codes can be found by adding 2^n to each value (in reverse order). For. eg.,:
+# n = 0 =>   0 (0)
+# n = 1 =>   1 (1)
+# n = 2 =>  11 (3)
+#           10 (2)
+# n = 3 => 110 (6)
+#          111 (7)
+#          101 (5)
+#          100 (4)

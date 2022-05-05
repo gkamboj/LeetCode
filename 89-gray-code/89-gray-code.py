e339d26416d98ed4b26e816c94f1f86815eab76c
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-06 01:05:30 +0530

    Time: 131 ms (70.20%), Space: 21.5 MB (41.68%) - LeetHub

--- 89-gray-code/89-gray-code.py
+++ 89-gray-code/89-gray-code.py
@@ -4,3 +4,7 @@ class Solution:
         for i in range(1 << n):
             result.append(i ^ (i // 2))
         return result
+    
+# Approach: G(i) = i ^ (i / 2)
+
+# Reference: https://leetcode.com/problems/gray-code/discuss/29881/An-accepted-three-line-solution-in-JAVA

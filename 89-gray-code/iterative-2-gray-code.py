cd228a5361e22202326fc6a026652dc700740517
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-06 10:28:42 +0530

    Delete iterative-2-gray-code.py

--- 89-gray-code/iterative-2-gray-code.py
+++
@@ -1,9 +0,0 @@
-class Solution:
-    def grayCode(self, n: int) -> List[int]:
-        result = []
-        for i in range(1 << n):
-            result.append(i ^ (i // 2))
-        return result
-    
-# Approach: G(i) = i ^ (i / 2)
-# Reference: https://leetcode.com/problems/gray-code/discuss/29881/An-accepted-three-line-solution-in-JAVA

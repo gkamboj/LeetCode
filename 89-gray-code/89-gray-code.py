eb58d295900e6dbc48c2ae879c7a9ef455136c61
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-06 10:27:42 +0530

    Time: 183 ms (33.07%), Space: 21.5 MB (41.68%) - LeetHub

---
+++ 89-gray-code/89-gray-code.py
@@ -0,0 +1,10 @@
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        result = []
+        for i in range(1 << n):
+            result.append(i ^ (i >> 1))
+        return result
+    
+# Approach: G(i) = i ^ (i / 2)
+
+# Reference: https://leetcode.com/problems/gray-code/discuss/29881/An-accepted-three-line-solution-in-JAVA

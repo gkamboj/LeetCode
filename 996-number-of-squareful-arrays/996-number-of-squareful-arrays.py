4d526c5b961db98700ee674f4964e45e6ca5dd8e
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-07 21:53:50 +0530

    Time: 41 ms (72.26%), Space: 13.8 MB (100.00%) - LeetHub

--- 996-number-of-squareful-arrays/996-number-of-squareful-arrays.py
+++ 996-number-of-squareful-arrays/996-number-of-squareful-arrays.py
@@ -11,9 +11,12 @@ class Solution:
         for i in range(len(nums)):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-            if perm and not self.isPerfectSquare(perm[-1] + nums[i]):
-                continue
-            self.numSquarefulRecursive(nums[:i] + nums[i + 1:], perm + [nums[i]], count)
+            if not perm or (perm and self.isPerfectSquare(perm[-1] + nums[i])):
+                self.numSquarefulRecursive(nums[:i] + nums[i + 1:], perm + [nums[i]], count)
         
     def isPerfectSquare(self, num):
         return int(num ** 0.5) ** 2 == num
+    
+# Approach: This is recursive DFS solution.
+
+# Reference: https://leetcode.com/problems/number-of-squareful-arrays/discuss/841254/Python-easy-to-understand-backtracking-solution-with-comments

05b4077ab126bc74cffe31e1e43a2ea056dd5548
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-08 22:31:20 +0530

    Time: 113 ms (63.05%), Space: 14 MB (37.29%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,41 +1,28 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        arr = []
-        for row in board:
-            for val in row:
-                if val != '.':
-                    arr.append(int(val))
-            if not self.isValid(sorted(arr)):
-                return False
-            arr = []
-            
-        for i in range(len(board[0])):
-            for row in board:
-                if row[i] != '.':
-                    arr.append(int(row[i]))
-            if not self.isValid(sorted(arr)):
-                return False
-            arr = []
-            
-        for rowInd in range(0, len(board), 3):
-            for colInd in range(0, len(board[0]), 3):
-                for row in range(rowInd, rowInd + 3):
-                    for col in range(colInd, colInd + 3):
-                        if board[row][col] != '.':
-                            arr.append(int(board[row][col]))
-                if not self.isValid(sorted(arr)):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if not self.isValid(board, i, j):
                     return False
-                arr = []
-                
         return True
             
         
         
-    def isValid(self, arr):
-        isValid = True
-        if len(arr) > 1:
-            for i in range(1, len(arr)):
-                if arr[i] == arr[i - 1]:
-                    isValid = False
-                    break
-        return isValid
+    def isValid(self, board, rowInd, colInd):
+        if board[rowInd][colInd] == '.':
+            return True
+        
+        for i in range(len(board)):
+            if i != rowInd and board[rowInd][colInd] == board[i][colInd]:
+                return False
+            
+        for i in range(len(board[0])):
+            if i != colInd and board[rowInd][colInd] == board[rowInd][i]:
+                return False
+            
+        for i in range(rowInd//3 * 3, rowInd//3 * 3 + 3):
+            for j in range(colInd//3 * 3, colInd//3 * 3 + 3):
+                if (i != rowInd or j != colInd) and board[rowInd][colInd] == board[i][j]:
+                    return False
+                
+        return True

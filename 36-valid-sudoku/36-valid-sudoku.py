acb9a6c8efa49b78b96648c741c63cb2bc20ecc0
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-08 23:09:12 +0530

    Time: 101 ms (80.61%), Space: 13.9 MB (84.06%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.py
+++ 36-valid-sudoku/36-valid-sudoku.py
@@ -1,6 +1,8 @@
+from collections import defaultdict
+
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        occured = []
+        occured = defaultdict(int)
         for i in range(len(board)):
             for j in range(len(board[0])):
                 val = board[i][j]
@@ -8,11 +10,11 @@ class Solution:
                     rowVal = str(i) + 'r-' + val
                     colVal = val + '-c' + str(j)
                     subSqVal = str(i // 3 * 3) + '-' + val + '-' + str(j // 3 * 3)
-                    if rowVal in occured or colVal in occured or subSqVal in occured:
+                    if occured[rowVal] or occured[colVal] or occured[subSqVal]:
                         return False
-                    occured.append(rowVal)
-                    occured.append(colVal)
-                    occured.append(subSqVal)
+                    occured[rowVal] = 1
+                    occured[colVal] = 1
+                    occured[subSqVal] = 1
         return True
         
         

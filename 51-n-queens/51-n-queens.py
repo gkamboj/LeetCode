f18d182d940ef5302c22551a9daca18aeb82896a
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-11 00:42:40 +0530

    Time: 149 ms (25.93%), Space: 14.4 MB (84.90%) - LeetHub

---
+++ 51-n-queens/51-n-queens.py
@@ -0,0 +1,58 @@
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        board = [['.' for i in range(n)] for j in range(n)]
+        result = []
+        self.solve(0, board, result)
+        return result
+        
+    def solve(self, row, board, result):
+        if row == len(board):
+            result.append([''.join(val) for val in board])
+            return
+        
+        for col in range(len(board[0])):
+            if self.isValid(row, col, board, len(board)):
+                board[row][col] = 'Q'
+                self.solve(row + 1, board, result)
+                board[row][col] = '.'
+        
+    
+    def isValid(self, i, j, board, n):
+        for c in board[i]:
+            if c == 'Q':
+                return False
+        
+        for c in range(n):
+            if board[c][j] == 'Q':
+                return False
+        
+        x, y = i, j
+        while x > 0 and y > 0:
+            x -= 1
+            y -= 1
+            if board[x][y] == 'Q':
+                return False
+        
+        x, y = i, j
+        while x < (n - 1) and y < (n - 1):
+            x += 1
+            y += 1
+            if board[x][y] == 'Q':
+                return False
+        
+        x, y = i, j
+        while x > 0 and y < (n - 1):
+            x -= 1
+            y += 1
+            if board[x][y] == 'Q':
+                return False
+            
+        x, y = i, j
+        while x < (n - 1) and y > 0:
+            x += 1
+            y -= 1
+            if board[x][y] == 'Q':
+                return False
+        
+        return True
+            

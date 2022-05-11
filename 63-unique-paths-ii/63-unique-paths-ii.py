d018d5d5a3e2edc3a792be5591c832082501e393
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-11 22:45:18 +0530

    Time: 45 ms (82.15%), Space: 13.9 MB (96.86%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.py
+++ 63-unique-paths-ii/63-unique-paths-ii.py
@@ -2,23 +2,20 @@ class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: List[List[int]]) -> int:
         if obstacleGrid[0][0] == 1:
             return 0
-        dp = [[0 for i in range(len(obstacleGrid[0]))] for j in range(len(obstacleGrid))]
         arr = [0 for i in range(len(obstacleGrid[0]))]
         for row in range(len(obstacleGrid)):
             for col in range(len(obstacleGrid[0])):
                 if row == 0 and col == 0:
-                    dp[row][col] = 1
                     arr[col] = 1
                 elif obstacleGrid[row][col] == 1:
-                    dp[row][col] = 0
                     arr[col] = 0        
                 elif row == 0:
-                    dp[row][col] = dp[row][col - 1]
+                    arr[col] = arr[col - 1]
                 elif col == 0:
-                    dp[row][col] = dp[row - 1][col]
+                    continue
                 else:
-                    dp[row][col] = dp[row - 1][col] + dp[row][col - 1]
-        return dp[-1][-1]
+                    arr[col] += arr[col - 1]
+        return arr[-1]
         
                     
 # Approach: This is DP approach, similar to DP submission of LC-62, with the change being setting the position to 0 if it's blocked. To avoid extra space, input grid array is being modified.

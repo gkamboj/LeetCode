5e1b9a6386a6c4cbbd8bb2e38c7f13059ae2f8b6
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-11 18:29:34 +0530

    Time: 52 ms (29.18%), Space: 13.7 MB (98.46%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -10,7 +10,7 @@ class Solution:
         return dp[-1][-1]
                     
     
-# Aproach: This is DP approach using a 2-D array s.t. dp[i][j] represents the number of ways of reching (i, j) from start. Observe that dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+# Aproach: This is DP approach using a 2-D array s.t. dp[i][j] represents the number of ways of reching (i, j) from start. Observe that dp[i][j] = dp[i - 1][j] + dp[i][j - 1].
 
 # Note: Below is a recursive solution, but it gives time limit exceeded for large inputs:
 #def uniquePaths(m, n):

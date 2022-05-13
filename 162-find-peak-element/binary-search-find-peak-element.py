482d9f3ee483e609671d456a277ba01f425612e7
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-13 22:31:16 +0530

    Time: 54 ms (68.05%), Space: 14 MB (84.99%)

--- 162-find-peak-element/binary-search-find-peak-element.py
+++ 162-find-peak-element/binary-search-find-peak-element.py
@@ -1,17 +1,17 @@
 class Solution:
     def findPeakElement(self, nums: List[int]) -> int:
-        low, high = 0, len(nums) - 1
-        while low < high:
-            mid = (low + high) // 2
-            if nums[mid] > nums[mid - 1] and nums[mid] > nums[mid + 1]:
-                return mid
-            elif nums[mid] < nums[mid + 1]:
-                low = mid + 1
+        start, end = 0, len(nums) - 1
+        while start < end:
+            mid = start + (end - start) // 2
+            if nums[mid] < nums[mid + 1]:
+                start = mid + 1
             else:
-                high = mid
-        return low
+                end = mid
+        return start
     
-#Approach: Since we need to find just one peak element (and not all), we can apply binary search. This works because if nums[mid] > nums[mid + 1], that means peak has to be on left: either whole array will be decreasing (nums[0] will be peak) or there will be dip somewhere on left side. Similarly when nums[mid] < nums[mid + 1], peak will be on right.
+#Approach: Since we need to find just one peak element (and not all), we can apply binary search. This works because if nums[mid] < nums[mid + 1]
+# that means peak has to be on right: either whole array will be increasing (nums[-1] will be peak as (n+1)th element can be assumed as infinity)
+# or there will be dip somewhere on right side. Similarly when nums[mid] > nums[mid + 1], peak will be on left.
 
 #Note: low <= high, high = (mid - 1) will not work.
 #Note: Use conditions low <= high and high = (mid - 1) only when we are not comparing with adjacent elements, else it'll cause index out of bound exception

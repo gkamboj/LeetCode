eecfc65a8fdaa0ea4e92fd5c44c5b43641da047e
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-13 22:36:08 +0530

    Time: 54 ms (68.05%), Space: 14 MB (84.99%)

--- 162-find-peak-element/binary-search-find-peak-element.py
+++ 162-find-peak-element/binary-search-find-peak-element.py
@@ -13,5 +13,8 @@ class Solution:
 # that means peak has to be on right: either whole array will be increasing (nums[-1] will be peak as (n+1)th element can be assumed as infinity)
 # or there will be dip somewhere on right side. Similarly when nums[mid] > nums[mid + 1], peak will be on left.
 
+# Note-1: end = mid is used (and not end = mid + 1) because when nums[mid] >= nums[mid + 1], it's possibility that index mid is our answer, but taking
+# end = mid - 1 will discard that possibility. Whereas for start, we are sure that mid index cannot be answer, but (mid + 1) can be, so start = mid + 1
+
 #Note: low <= high, high = (mid - 1) will not work.
 #Note: Use conditions low <= high and high = (mid - 1) only when we are not comparing with adjacent elements, else it'll cause index out of bound exception

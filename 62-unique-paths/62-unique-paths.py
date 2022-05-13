c18f80ce4f45d5b769010af3eddb157706d32fcb
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-13 23:06:53 +0530

    Time: 70 ms (5.93%), Space: 14 MB (14.89%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -5,3 +5,5 @@ class Solution:
             for col in range(1, n):
                 arr[col] += arr[col - 1]
         return arr[-1]
+    
+# Approcah: This is DP approach using 1-D array. This follows same logic as other DP submission but using 1-D array only. Observe that at any time, we need to access just the left and top element and that can be done using 1-D array only: left element is preceding element and top element is just the current value at current index.

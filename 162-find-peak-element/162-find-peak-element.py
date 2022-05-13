750eb2c9491521fe81000e5e3f7360075985349b
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-13 22:32:29 +0530

    Time: 54 ms (67.55%), Space: 14 MB (45.70%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.py
+++ 162-find-peak-element/162-find-peak-element.py
@@ -3,8 +3,8 @@ class Solution:
         start, end = 0, len(nums) - 1
         while start < end:
             mid = start + (end - start) // 2
-            if nums[mid] < nums[mid + 1]:
-                start = mid + 1
-            else:
+            if nums[mid] > nums[mid + 1]:
                 end = mid
+            else:
+                start = mid + 1
         return start

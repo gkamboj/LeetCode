d63ab6bc3a7590790b19781da79f005277d324e9
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-13 23:07:40 +0530

    Time: 44 ms (48.80%), Space: 14 MB (32.93%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -6,4 +6,4 @@ class Solution:
                 arr[col] += arr[col - 1]
         return arr[-1]
     
-# Approcah: This is DP approach using 1-D array. This follows same logic as other DP submission but using 1-D array only. Observe that at any time, we need to access just the left and top element and that can be done using 1-D array only: left element is preceding element and top element is just the current value at current index.
+# Approcah: This is DP approach using 1-D array. This follows same logic as other DP submission but using 1-D array only. Observe that at any time, we need to access just the left and top element and that can be done using 1-D array only: left element is preceding element and top element is just the current value at current index. See explanation of 2-D DP submission for more detail on logic.

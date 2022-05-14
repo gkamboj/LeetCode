2cdaf66106839e68fedc86f0f4cd4dba146f54ae
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-14 21:59:42 +0530

    Time: 108 ms (56.74%), Space: 14.2 MB (49.30%)

--- 768-max-chunks-to-make-sorted-ii/sort-sum-max-chunks-to-make-sorted-ii.py
+++ 768-max-chunks-to-make-sorted-ii/sort-sum-max-chunks-to-make-sorted-ii.py
@@ -1,13 +1,10 @@
 class Solution:
     def maxChunksToSorted(self, arr: List[int]) -> int:
-        sortedArr = arr[:]
-        sortedArr.sort()
-        arrSum, sortedArrSum, chunks = 0, 0, 0
+        sortedArr, chunks, sortedSum, currSum = sorted(arr)[:], 0, 0, 0
         for i in range(len(arr)):
-            arrSum += arr[i]
-            sortedArrSum += sortedArr[i]
-            if arrSum == sortedArrSum:
-                chunks += 1
+            currSum += arr[i]
+            sortedSum += sortedArr[i]
+            chunks += int(currSum == sortedSum)
         return chunks
     
 #Approach: Create an auxiliary sorted array. Iterate over arrays and store sum of both arrays. Increase the counter when both sums become equal.

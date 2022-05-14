d6b7e90553e156c10a6dd0c741aab7cab99a8a78
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-15 01:04:04 +0530

    Time: 36 ms (80.97%), Space: 13.8 MB (94.48%)

--- 190-reverse-bits/190-reverse-bits.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def reverseBits(self, n: int) -> int:
-        ans = 0
-        for i in range(32):
-            ans = (ans | ((n >> i) & 1)) << 1
-        return ans >> 1
-    
-# Approach: Get ith bit and add it to the answer, then shift answer towards left by 1 bit (so that next iteration's ith bit is added at list). While returning, right shift by 1 bit because answer is left shifted one time extra in for loop.

3aa05f06ce3d97f6b2360653b64351928ae2cac8
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-14 22:14:59 +0530

    Time: 114 ms (51.95%), Space: 14.3 MB (49.43%)

--- 768-max-chunks-to-make-sorted-ii/right-min-max-chunks-to-make-sorted-ii.py
+++ 768-max-chunks-to-make-sorted-ii/right-min-max-chunks-to-make-sorted-ii.py
@@ -11,4 +11,8 @@ class Solution:
                 chunks += 1
         return chunks
     
-#Approach: Create an auxiliary array to store minimum value from end at every index. Travers array from start and keep updating maximum value till now. If this max value is less than the right min, then increment the chunks. Note that chunks has been initialsed to 1 (and not 0) as there will be atleast one chunk of whole array even if our condition is never satisfied. O(N) space & O(N) time.
+#Approach: Create an auxiliary array to store minimum value from end at every index. Travers array from start and keep updating maximum value till now.
+# If this max value is less than the right min, then increment the chunks. Note that chunks has been initialsed to 1 (and not 0) as there will be atleast
+# one chunk of whole array even if our condition is never satisfied. O(N) space & O(N) time.
+# This works because when we traverse A, we can split A at index i into A[:i+1] and A[i+1:] if and only if max(A[:i+1]) <= min(A[i+1:]). This to maintain
+# the invariant that sorted(A[:i+1]) + sorted(A[i+1:]) == sorted(A).

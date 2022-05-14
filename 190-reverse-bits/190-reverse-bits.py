f505a5ca5aa0c4ec877761694756156c5d7a0e5f
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-15 01:03:44 +0530

    Time: 36 ms (80.97%), Space: 13.8 MB (94.48%) - LeetHub

--- 190-reverse-bits/190-reverse-bits.py
+++ 190-reverse-bits/190-reverse-bits.py
@@ -5,4 +5,4 @@ class Solution:
             ans = (ans | ((n >> i) & 1)) << 1
         return ans >> 1
     
-# Approach: Get ith bit and add it to the answer, then shift answer towards left by 1 bit (so that next iteration's ith bit is added at list). While returning, right shift by 1 bit because answer is left shifted one time extra.
+# Approach: Get ith bit and add it to the answer, then shift answer towards left by 1 bit (so that next iteration's ith bit is added at list). While returning, right shift by 1 bit because answer is left shifted one time extra in for loop.

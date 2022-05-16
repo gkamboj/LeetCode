20984cb204b3c2a23b5c704effb25635a1ded6f7
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-17 00:36:39 +0530

    Time: 109 ms (38.13%), Space: 14.1 MB (76.94%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -15,3 +15,7 @@ class Solution:
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
             self.permute(nums[:i] + nums[i + 1:], perm + [nums[i]], result)
+            
+# Approach: This is recursive DFS approach. Pass the sorted array to recursive method. For every index, if current index element is not equal to previous element then call the method again recursively by updating perm.
+
+# Reference: https://leetcode.com/problems/permutations-ii/discuss/18649/Python-easy-to-understand-backtracking-solution.

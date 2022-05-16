0d98098d7d972499cf49cdff31352f0f7a134850
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-17 00:31:29 +0530

    Time: 60 ms (92.32%), Space: 14.1 MB (94.53%) - LeetHub

--- 47-permutations-ii/47-permutations-ii.py
+++ 47-permutations-ii/47-permutations-ii.py
@@ -2,13 +2,22 @@ from collections import defaultdict
 
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
-        result = [[]]
-        for num in nums:
-            perms = []
-            for perm in result:
-                for i in range(len(perm) + 1):
-                    perms.append(perm[:i] + [num] + perm[i:])
-                    if i < len(perm) and perm[i] == num:
-                        break
-            result = perms
+        return self.permute(sorted(nums))
+        
+    def permute(self, nums):
+        if len(nums) == 1:
+            return [[nums[0]]]
+        
+        result, perv = [], -11
+        
+        for i in range(len(nums)):
+            num = nums.pop(0)
+            if num != perv:
+                perms = self.permute(nums)
+                for perm in perms:
+                    perm.append(num)
+                result += perms
+            nums.append(num)
+            perv = num
+        
         return result

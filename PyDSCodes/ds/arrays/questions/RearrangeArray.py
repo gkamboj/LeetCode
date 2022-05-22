4fbc6c0a15b7e2fbd5f1849f42f38b4aab3f738e
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-22 22:50:56 +0530

    Arrays: Rearrange array new approach

--- PyDSCodes/ds/arrays/questions/RearrangeArray.py
+++ PyDSCodes/ds/arrays/questions/RearrangeArray.py
@@ -10,11 +10,26 @@ def rearrangeArray(arr):
     return arr
 
 
+def rearrangeArray2(arr):
+    n = len(arr)
+    for i in range(n):
+        arr[i] *= n
+    for i in range(n):
+        arr[arr[i] // n] = arr[arr[i] // n] * n + i
+    for i in range(n):
+        arr[i] %= n
+    return arr
+
+
 arr = [2, 0, 1, 4, 5, 3]
+arr2 = arr[:]
 # 1 2 0 5 3 4
 print(rearrangeArray(arr))
+print(rearrangeArray2(arr2))
 
-# Approach: The idea is to store each element’s new and old value as quotient and remainder of n, respectively (n
+# Approach-1: The idea is to store each element’s new and old value as quotient and remainder of n, respectively (n
 # being the size of the array). Instead of using arr[arr[ind]] in line-7, arr[arr[ind] % n] is used because some of
 # the elements maybe having the updated value when we reach that ind and accessing arr[arr[ind]] will give index out
 # of bound exception.
+
+# Approach-2: Similar to first approach, but here the ans is stored as remainder instead of quotient.

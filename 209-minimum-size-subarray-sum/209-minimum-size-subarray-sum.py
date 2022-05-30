06acc6bae905fff6138c97f8704c01f01cd57538
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-31 00:28:56 +0530

    Time: 102 ms (52.08%), Space: 16.8 MB (50.74%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -8,3 +8,5 @@ class Solution:
                 curr -= nums[start]
                 start += 1
         return ans if ans != len(nums) + 1 else 0
+    
+# Approach: This is 2-pointer approach. Keep moving end pointer till sum becomes more than target. Then, begin moving start pointer till sum becomes less than target, updating ans all along. TC: O(n) as each element can be visited at max twice: once each by start and end pointer, Space: O(1)

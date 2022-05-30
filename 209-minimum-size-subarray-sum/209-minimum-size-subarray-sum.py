af84688530b25b5ae0e3ac4837cc31d46e8745fe
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-05-30 23:06:50 +0530

    Time: 178 ms (9.50%), Space: 16.8 MB (50.74%) - LeetHub

---
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.py
@@ -0,0 +1,22 @@
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        start, end, ans = 1, len(nums), 0
+        while start <= end:
+            mid = start + (end - start) // 2
+            if self.isValidSize(mid, target, nums):
+                ans = mid
+                end = mid - 1
+            else:
+                start = mid + 1
+        return ans
+    
+    def isValidSize(self, size, target, arr):
+        curr, ans = 0, False
+        for i in range(size):
+            curr += arr[i]
+        for i in range(size, len(arr)):
+            if curr >= target:
+                ans = True
+                break
+            curr += arr[i] - arr[i - size]
+        return True if curr >= target else ans

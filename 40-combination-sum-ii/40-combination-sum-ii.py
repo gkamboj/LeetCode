58c4475d90bc3cc74851593299b32147eb86795f
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-06-13 23:24:03 +0530

    Time: 71 ms (81.54%), Space: 13.9 MB (92.78%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.py
+++ 40-combination-sum-ii/40-combination-sum-ii.py
@@ -1,17 +1,17 @@
 class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         result = []
-        self.solve(sorted(candidates), target, [], result)
+        self.solve(sorted(candidates), target, [], result, 0)
         return result
     
-    def solve(self, nums, target, comb, result):
+    def solve(self, nums, target, comb, result, ind):
         if target == 0:
             result.append(comb)
             return
         
-        for i in range(len(nums)):
-            if i > 0 and nums[i] == nums[i - 1]:
+        for i in range(ind, len(nums)):
+            if i > ind and nums[i] == nums[i - 1]:
                 continue
             if nums[i] > target:
                 break
-            self.solve(nums[i + 1:], target - nums[i], comb + [nums[i]], result)
+            self.solve(nums, target - nums[i], comb + [nums[i]], result, i + 1)

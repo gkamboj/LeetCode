b7fe008e851667bd3c5cd98c978023e58caa2a60
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-06-13 19:32:55 +0530

    Time: 141 ms (34.92%), Space: 14 MB (63.56%) - LeetHub

---
+++ 39-combination-sum/39-combination-sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        result = []
+        self.solve(candidates, result, [], 0, target)
+        return result
+    
+    def solve(self, nums, result, comb, ind, target):
+        if ind >= len(nums):
+            if target == 0:
+                result.append(comb)
+            return
+        if target < 0:
+            return
+        if nums[ind] <= target:
+            self.solve(nums, result, comb + [nums[ind]], ind, target - nums[ind])
+        self.solve(nums, result, comb, ind + 1, target)
+        
+        
+# Arpproach: This approach is based on TUF's subsequences approach. Refer https://youtu.be/OyZFFqQtu98 for detail.

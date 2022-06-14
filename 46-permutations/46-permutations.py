d18d0f1f9bf082d8f99cc8f8eaa4d7c5314a016b
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-06-14 23:27:41 +0530

    Time: 68 ms (27.95%), Space: 14 MB (57.61%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -4,13 +4,15 @@ class Solution:
         self.solve(nums, 0, result)
         return result
     
-    
     def solve(self, nums, ind, result):
         if ind == len(nums):
             result.append(nums[:])
             return
         
         for i in range(ind, len(nums)):
-            nums[i], nums[ind] = nums[ind], nums[i]
+            nums[ind], nums[i] = nums[i], nums[ind]
             self.solve(nums, ind + 1, result)
-            nums[i], nums[ind] = nums[ind], nums[i]
+            nums[ind], nums[i] = nums[i], nums[ind]
+            
+            
+# Approach: For every number, find permutations starting with that number, Repeat recursively for every position. Refer https://youtu.be/f2ic2Rsc9pU for detail.

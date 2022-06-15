6293ffe3871ce79f6ea0de651ad710dbb0af2052
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-06-15 22:34:36 +0530

    Time: 150 ms (26.07%), Space: 14.5 MB (12.83%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -3,20 +3,18 @@ from collections import defaultdict
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         board = [['.' for i in range(n)] for j in range(n)]
-        result, d = [], defaultdict(int)
-        self.solve(board, result, 0, d)
+        result, dictMap = [], defaultdict(int)
+        self.solve(board, 0, result, dictMap)
         return result
     
-    def solve(self, board, result, row, d):
+    def solve(self, board, row, result, d):
         if row == len(board):
             result.append([''.join(val) for val in board])
             return
-            
-        for col in range(len(board[0])):
-            if not d['c' + str(col)] and not d['r' + str(row + col)] and not d['l' +str(len(board) - col - 1 + row)]:
-                d['c' + str(col)], d['r' + str(row + col)], d['l' +str(len(board) - col - 1 + row)] = 1, 1, 1
+        for col in range(len(board)):
+            if not d['c' + str(col)] and not d['r' + str(row + col)] and not d['l' + str(len(board) - col - 1 + row)]:
+                d['c' + str(col)], d['r' + str(row + col)], d['l' + str(len(board) - col - 1 + row)] = 1, 1, 1
                 board[row][col] = 'Q'
-                self.solve(board, result, row + 1, d)
-                d['c' + str(col)], d['r' + str(row + col)], d['l' +str(len(board) - col - 1 + row)] = 0, 0, 0
+                self.solve(board, row + 1, result, d)
                 board[row][col] = '.'
-                
+                d['c' + str(col)], d['r' + str(row + col)], d['l' + str(len(board) - col - 1 + row)] = 0, 0, 0

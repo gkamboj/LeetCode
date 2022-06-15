e77210e68f4adf953600704680b0d4a45f3e0b06
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-06-15 22:38:40 +0530

    Time: 114 ms (29.94%), Space: 13.9 MB (40.52%)

--- 52-n-queens-ii/backtracking-map-n-queens-ii.py
+++ 52-n-queens-ii/backtracking-map-n-queens-ii.py
@@ -4,25 +4,24 @@ class Solution:
     def totalNQueens(self, n: int) -> int:
         board = [['.' for i in range(n)] for j in range(n)]
         d, count = defaultdict(int), [0]
-        self.solve(0, board, d, count)
-        return count[0]
+        return self.solve(0, board, d, count)
         
     def solve(self, row, board, d, count):
         if row == len(board):
-            count[0] += 1
-            return
-        
+            return 1
+        ans = 0
         for col in range(len(board[0])):
             if not d['c' + str(col)] and not d['r' + str(row + col)] and not d['l' + str(row - col - 1 + len(board))]:
                 board[row][col] = 'Q'
                 d['c' + str(col)] = 1
                 d['r' + str(row + col)] = 1
                 d['l' + str(row - col - 1 + len(board))] = 1
-                self.solve(row + 1, board, d, count)
+                ans += self.solve(row + 1, board, d, count)
                 board[row][col] = '.'
                 d['c' + str(col)] = 0
                 d['r' + str(row + col)] = 0
                 d['l' + str(row - col - 1 + len(board))] = 0
+        return ans
                 
                 
 # Approach: This is backtracking approach. Refer LC-51 for explanation.

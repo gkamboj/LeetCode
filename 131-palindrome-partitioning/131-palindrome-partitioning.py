74b688e6ef5d5bab1501cbb49c43d3a5f937b0a0
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-06-16 00:27:16 +0530

    Time: 1064 ms (25.85%), Space: 29 MB (78.53%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.py
+++ 131-palindrome-partitioning/131-palindrome-partitioning.py
@@ -1,28 +1,30 @@
+from collections import defaultdict
+
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
-        result = []
-        self.dfs(s, result, [])
+        result, d = [], defaultdict(int)
+        self.solve(d, s, result, [])
         return result
     
-    def dfs(self, s, result, part):
+    def solve(self, d, s, result, pal):
         if not s:
-            result.append(part)
+            result.append(pal)
             return
-        
         for i in range(len(s)):
-            if self.isPalindrome(s[:i+1]):
-                self.dfs(s[i+1:], result, part + [s[:i+1]])
-        
+            if self.isPalindrome(s[:i + 1], d):
+                self.solve(d, s[i + 1:], result, pal + [s[:i+1]])
     
-    
-    def isPalindrome(self, s):
-        if len(s) == 1:
-            return True
-        start, end = 0, len(s) - 1
-        while start <= end:
-            if s[start] == s[end]:
-                start += 1
-                end -= 1
+    def isPalindrome(self, s, d):
+        if s in d:
+            result = d[s]
+        elif len(s) <= 1:
+            d[s] = True
+            result = True
+        else:
+            if s[0] == s[-1]:
+                result = self.isPalindrome(s[1:-1], d)
             else:
-                return False
-        return True
+                result = False
+        d[s] = result
+        return result
+            

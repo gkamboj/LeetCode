e3bb17a523ef25e6c1a7d02092cdd40937b68ae3
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-06-23 20:50:47 +0530

    Time: 113 ms (89.93%), Space: 15.8 MB (96.01%)

--- 77-combinations/recursive-dfs-combinations.py
+++ 77-combinations/recursive-dfs-combinations.py
@@ -9,7 +9,10 @@ class Solution:
             result.append(comb)
             return
         
-        for i in range(len(arr)):
-            self.solve(arr[i + 1:], comb + [arr[i]], k, result)
-            
+        if len(arr) + len(comb) >= k:
+            for i in range(len(arr)):
+                self.solve(arr[i + 1:], comb + [arr[i]], k, result)
+    
 # Approach: This is recursive DFS approach similar to LC-39 (Combination Sum) & LC-40 (Combination Sum 2) submissions. Refer https://leetcode.com/problems/combinations/discuss/26990/Python-easy-to-understand-DFS-solution for detail.
+
+# Note: Observe the if condition at line-12: This is to avoid for loop recursive calls for steps when combination of k is not possible, thereby improving time further.

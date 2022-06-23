e555a184e9a2d2fd282be49abca1c9d44c2828a5
Gaurav Kamboj <gauravkambojtu@gmail.com>
2022-06-23 20:37:37 +0530

    Time: 848 ms (13.50%), Space: 15.8 MB (84.66%) - LeetHub

--- 77-combinations/77-combinations.py
+++ 77-combinations/77-combinations.py
@@ -11,3 +11,5 @@ class Solution:
         
         for i in range(len(arr)):
             self.solve(arr[i + 1:], comb + [arr[i]], k, result)
+            
+# Approach: This is recursive DFS approach similar to LC-39 (Combination Sum) & LC-40 (Combination Sum 2) submissions. Refer https://leetcode.com/problems/combinations/discuss/26990/Python-easy-to-understand-DFS-solution for detail.

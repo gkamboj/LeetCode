b1da575a382459a26669bac14fcc545249c30aa2
Gaurav Kamboj <gauravkambojtu@gmail.com>
2023-05-15 00:18:21 +0530

    Added more detail to approach

--- 0768-max-chunks-to-make-sorted-ii/sort-sum-max-chunks-to-make-sorted-ii.py
+++ 0768-max-chunks-to-make-sorted-ii/sort-sum-max-chunks-to-make-sorted-ii.py
@@ -8,3 +8,5 @@ class Solution:
         return chunks
     
 #Approach: Create an auxiliary sorted array. Iterate over arrays and store sum of both arrays. Increase the counter when both sums become equal.
+#This works because once array is sorted, sum till a given index i can only be same if original array also contains same elements as sorted array.
+#If original array was to contain different elements (which means greater elements than the sorted part), then sum would never have been same.

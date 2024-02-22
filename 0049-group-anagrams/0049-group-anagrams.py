0bbfd6d0eb8ff8d073f52538a7cad9a69666274e
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-02-23 02:06:50 +0530

    Time: 95 ms (38.19%), Space: 20.1 MB (66.92%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -4,3 +4,5 @@ class Solution:
         for word in strs:
             result[''.join(sorted(word))].append(word)
         return result.values()
+    
+# Approach: Create a dictionary with key as sorted string of every element, so value for every key will be a list of anagram strings. 

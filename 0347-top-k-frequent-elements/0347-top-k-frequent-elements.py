0c3c165ca1de2286290286e8982cb01f2e481d8d
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-02-27 03:16:11 +0530

    Time: 98 ms (25.93%), Space: 22.7 MB (12.60%) - LeetHub

---
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -0,0 +1,17 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        freqDict = defaultdict(int)
+        for num in nums:
+            freqDict[num] += 1
+        if len(freqDict) == k:
+            return freqDict.keys()
+        else:
+            result = []
+            freqList = [[] for i in range(len(nums))]
+            for key, val in freqDict.items():
+                freqList[val - 1].append(key)
+            for ls in freqList[::-1]:
+                if len(result) < k:
+                    result += ls
+                else:
+                    return result

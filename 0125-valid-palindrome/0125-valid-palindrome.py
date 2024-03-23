a3bcc51a40d958a8bc19afb3f4cef95625a88cc3
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-03-24 00:41:50 +0530

    Time: 46 ms (51.33%), Space: 17.1 MB (47.10%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,6 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        print(s)
         start, end = 0, len(s)  - 1
         while start <= end:
             if not s[start].isalpha() and not s[start].isnumeric():
@@ -14,4 +13,6 @@ class Solution:
             start += 1
             end -= 1
         return True
+    
+# Approach: Change the string to lower, and check for palindrome along with conditions of the question.
     

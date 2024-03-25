d86a5ca4a930338521339a4ad7afdecf9318e296
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-03-25 23:09:13 +0530

    Time: 39 ms (50.95%), Space: 16.8 MB (42.99%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -2,7 +2,6 @@ class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
         stack, result = [], []
         
-        
         def backtrack(openP, closeP):
             if len(stack) == 2*n:
                 result.append(''.join(stack))
@@ -20,3 +19,10 @@ class Solution:
         backtrack(0, 0)
         return result
         
+# Approach:
+# 1. Add open bracket if open bracket count < n
+# 2. Add closed bracket if closed bracket count < open count
+# 3. Add to result if stack size = 2n
+# Pop from stack after because same stack variable is not a paramter to the backtrack
+# method, same variable is used for other operations.
+# Detail: https://www.youtube.com/watch?v=s9fokUqJ76A

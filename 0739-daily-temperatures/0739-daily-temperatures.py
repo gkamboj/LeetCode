b3df0fc04274f587eb123d89ce48dcec6e86f85f
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-03-26 00:39:21 +0530

    Time: 891 ms (63.28%), Space: 30.2 MB (84.51%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.py
+++ 0739-daily-temperatures/0739-daily-temperatures.py
@@ -2,8 +2,11 @@ class Solution:
     def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
         result, stack = [0 for i in range(len(temperatures))], []
         for ind, temp in enumerate(temperatures):
-            while stack and stack[-1][-1] < temp:
-                top_ind = stack.pop()[0]
+            while stack and temperatures[stack[-1]] < temp:
+                top_ind = stack.pop()
                 result[top_ind] = ind - top_ind
-            stack.append([ind, temp])
+            stack.append(ind)
         return result
+    
+# Approach: Using stack, store currently unsolved elements. When encountered higher temp, pop all smaller temps
+# and update the answer. Solve more problems on monotonic stack for better clarity: https://www.notion.so/Stacks-d0ba7936119243e0993895078b96ebaf?pvs=4

580590c065d5bbe6a6b38b2734eeeb73f9cefc59
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-03-26 11:57:29 +0530

    Time: 665 ms (74.76%), Space: 38.3 MB (60.61%) - LeetHub

--- 0853-car-fleet/0853-car-fleet.py
+++ 0853-car-fleet/0853-car-fleet.py
@@ -8,3 +8,10 @@ class Solution:
                 fleets += 1
         return fleets
         
+# Approach: Since road is single lane and overtaking is not possible, we have to start from the car with
+# highest position (say X) even if there are faster car beyond that. This is because each car has to either
+# join the X's fleet or make separate fleet. If any car's time to target is less than the X's time, those will
+# meet before reaching target and hence will be part of same fleet. If any car's time to target is more than
+# current time (X's time), then this car will have separate fleet. This will be repeated for all cars to get
+# total number of fleets.
+# See this for visual representation: https://www.youtube.com/watch?v=H5w6doOXz10

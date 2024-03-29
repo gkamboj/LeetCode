aab5d8ddb64418b294b9307886ea8956bb938d52
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-03-30 00:29:55 +0530

    Time: 26 ms (95.51%), Space: 16.5 MB (64.78%)

--- 0278-first-bad-version/0278-binary_search_v1-first-bad-version.py
+++ 0278-first-bad-version/0278-binary_search_v1-first-bad-version.py
@@ -9,7 +9,7 @@ class Solution:
             mid = left + (right - left) // 2
             if not isBadVersion(mid):
                 left = mid + 1
-            elif mid == 1 or not isBadVersion(mid - 1):
+            elif not isBadVersion(mid - 1):
                 return mid
             else: right = mid - 1
 

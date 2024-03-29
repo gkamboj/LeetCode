92343d838bddfba5ff065db6838ef8fa951bee60
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-03-29 23:39:45 +0530

    Time: 859 ms (79.01%), Space: 30.8 MB (80.15%) - LeetHub

--- 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
+++ 2962-count-subarrays-where-max-element-appears-at-least-k-times/2962-count-subarrays-where-max-element-appears-at-least-k-times.py
@@ -6,12 +6,17 @@ class Solution:
             if nums[ind] == max_val:
                 count += 1
                 if count == 1: start = ind
+            
             while count > k:
                 start += 1
                 if nums[start] == max_val:
                     count -= 1
                 
             if count == k:
-                print(start, ind)
                 ans += start + 1
         return ans
+    
+# Approach: Sliding window -> Maintain a start pointer and end pointer so that frequency of max element b/w
+# these indices (both included) is k. Whenever end points to max element, add (start + 1) to answer as no.
+# of subarrays including start and end indices will be (start + 1): 'start' no. of subarrays starting before
+# 'start' index and another [start, end]

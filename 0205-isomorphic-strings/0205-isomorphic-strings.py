54647be3b9495c2cc63ecff7c52863e447c317b5
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-04-02 14:04:20 +0530

    Time: 39 ms (77.41%), Space: 16.8 MB (64.59%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.py
+++ 0205-isomorphic-strings/0205-isomorphic-strings.py
@@ -1,12 +1,13 @@
 class Solution:
     def isIsomorphic(self, s: str, t: str) -> bool:
+        if len(set(s)) != len(set(t)): return False
         char_dict = {}
         for ind in range(len(s)):
-            if t[ind] in char_dict:
-                if char_dict[t[ind]] != s[ind]:
-                    return False
-            else:
-                if s[ind] in char_dict.values():
-                    return False
+            if t[ind] not in char_dict:
                 char_dict[t[ind]] = s[ind]
+            elif char_dict[t[ind]] != s[ind]:
+                return False
         return True
+    
+""" Approach: Create a map to store mapping of t's chars with chars of s. If at anytime, s[ind] does not
+match with existing value of dict[t[ind]], return False """

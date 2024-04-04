187f578922c45e107e725efb914683626be73b88
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-04-05 02:08:50 +0530

    Time: 31 ms (80.46%), Space: 16.5 MB (38.23%) - LeetHub

--- 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.py
@@ -1,10 +1,13 @@
 class Solution:
     def maxDepth(self, s: str) -> int:
-        stack, ans = [], 0
+        ans, count = 0, 0
         for i in s:
             if i == '(':
-                stack.append(i)
+                count += 1
             elif i == ')':
-                stack.pop()
-            ans = max(ans, len(stack))
+                count -= 1
+            ans = max(ans, count)
         return ans
+    
+''' Approach: Given that input is VPS, we don't need to check for the validity of the input string. So, this
+can be done without using extra space of stack (needed for checking the if input is VPS) '''

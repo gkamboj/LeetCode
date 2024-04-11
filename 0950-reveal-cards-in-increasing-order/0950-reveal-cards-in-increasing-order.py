ea18207a0b4189f4913b608815d3da30ef3a7272
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-04-11 14:08:40 +0530

    Time: 43 ms (74.72%), Space: 16.8 MB (36.91%) - LeetHub

--- 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
+++ 0950-reveal-cards-in-increasing-order/0950-reveal-cards-in-increasing-order.py
@@ -1,26 +1,15 @@
 class Solution:
     def deckRevealedIncreasing(self, deck: List[int]) -> List[int]:
-        N = len(deck)
-        result = [0] * N
-        skip = False
-        index_in_deck = 0
-        index_in_result = 0
-
+        n = len(deck)
+        queue = deque([i for i in range(n)])
+        result = [0]*n
         deck.sort()
-
-        while index_in_deck < N:
-            # There is an available gap in result
-            if result[index_in_result] == 0:
-
-                # Add a card to result
-                if not skip:
-                    result[index_in_result] = deck[index_in_deck]
-                    index_in_deck += 1
-
-                # Toggle skip to alternate between adding and skipping cards
-                skip = not skip
-
-            # Progress to the next index of result array
-            index_in_result = (index_in_result + 1) % N
-
+        for card in deck:
+            result[queue.popleft()] = card
+            if queue: queue.append(queue.popleft())
         return result
+    
+'''Approach: Maintain a queue of indices to know the current index to be filled. After every filled index, 
+move the next index to the end of queue (as is the condition of question). Refer approach-2 of official
+solution for more detail.
+'''

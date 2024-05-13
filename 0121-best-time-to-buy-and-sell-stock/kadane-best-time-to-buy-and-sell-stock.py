5740fca3b602636dafa55eacc564757844c876d5
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-05-14 02:35:29 +0530

    Update and rename cancel-best-time-to-buy-and-sell-stock.py to kadane-best-time-to-buy-and-sell-stock.py

--- 0121-best-time-to-buy-and-sell-stock/kadane-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/kadane-best-time-to-buy-and-sell-stock.py
@@ -10,6 +10,7 @@ class Solution:
         return ans
     
 #Approach: This is based on Kadane's algorithm -> Assume an imaginary array containing difference of consecutive prices as the elements. Now, finding maximum sum subarray will give the max profit.
+#Refer for detail: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/discuss/39038/Kadane's-Algorithm-Since-no-one-has-mentioned-about-this-so-far-%3A)-(In-case-if-interviewer-twists-the-input)
 #e.g. 7, 1, 5, 3, 6, 4 -> 0, -6, 4, -2, 3, -2 => max sum subarray = [4, -2, 3] => profit = sum = 5
 
 #See other submissions for other approaches

45234979fe6d08518fa20efd296554a4c3a3d904
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-05-14 02:30:46 +0530

    Time: 736 ms (65.40%), Space: 27.4 MB (72.10%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.py
@@ -1,7 +1,16 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        profit, buy, sell = 0, max(prices), min(prices)
-        for price in prices:
-            buy = min(buy, price)
-            profit = max(profit, price - buy)
+        profit, buy, sell = 0, 0, 1
+        while sell < len(prices):
+            if prices[buy] < prices[sell]:
+                profit = max(profit, prices[sell] - prices[buy])
+            else:
+                buy = sell
+            sell += 1
         return profit
+            
+'''
+Approach: This is sliding window approach. Refer for more detail: https://youtu.be/1pkOgXD63yU
+
+Check other submissions for more approaches
+'''

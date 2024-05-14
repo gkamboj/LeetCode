b3b6348ee43c045fed6b82537cdf28eb0ade243f
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-05-15 03:11:43 +0530

    Time: 53 ms (68.61%), Space: 16.7 MB (60.42%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -3,8 +3,15 @@ class Solution:
         ans, start, counter = 0, 0, defaultdict(int)
         for ind, char in enumerate(s):
             if char in counter and counter[char] >= start:
+                # Characters before start index are irrelevant, so update start only if repeated char
+                # is after index start
                 start = counter[char] + 1
-            else:
-                ans = max(ans, ind - start + 1)
+            # else:
+            ans = max(ans, ind - start + 1)
             counter[char] = ind
         return ans
+    
+'''
+Approach: Use sliding window with start pointer and current pointer. Use dictionary to store last index 
+occurence of character. Whenever a character is repeated, update the start pointer.
+'''

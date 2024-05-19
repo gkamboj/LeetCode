63dfda78b43e29ac486eed9c7e90ba98d3343e11
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-05-19 19:12:17 +0530

    Time: 41 ms (80.51%), Space: 19.2 MB (34.18%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        try:
+            slow, fast = head, head.next
+            while slow != fast:
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except:
+            return False
+    
+'''
+Approach: Using EAFP (Easier to ask for forgiveness than permission): take slow and fast pointers starting from head. If they, ever meet before fast reaching end, cycle exists. If there's any exception (which will come when fast is None), return False.
+Read Floyd cycle detection algorithm for more detail.
+'''

2e998b35ffa21075270414c94b3587c56589284b
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-06-01 23:45:00 +0530

    Time: 45 ms (60.38%), Space: 17.4 MB (97.11%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -18,3 +18,16 @@ class Solution:
         
         dfs(root)
         return self.ans
+    
+    
+'''
+Definition: A tree is height balance if difference of heights of left and right sub tree is at most 1 for 
+every node of the tree.
+
+Approach: Bottom-up approach to recursively check the height of left as well as right subtrees for every node
+using DFS. Maintain a global boolean to check for height balance.
+
+* To avoid global variable, and for ITERATIVE approach, refer this link: https://leetcode.com/problems/balanced-binary-tree/discuss/35708/VERY-SIMPLE-Python-solutions-(iterative-and-recursive)-both-beat-90
+
+* Refer NC solution video for detailed theoretical explanation on how this is O(n) slution
+'''

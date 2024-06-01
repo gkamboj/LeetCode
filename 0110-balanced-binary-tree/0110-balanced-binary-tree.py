51906cdf2fb6e7a8ddad82ca72c811163a52f90e
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-06-02 00:01:58 +0530

    Time: 39 ms (88.29%), Space: 17.6 MB (83.43%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        return self.helper(root) != -1
+        
+    def helper(self, node):
+        if not node:
+            return 0
+        left = self.helper(node.left)
+        if left == -1:
+            return -1
+        right = self.helper(node.right)
+        if right == -1 or abs(left - right) > 1:
+            return -1
+        return max(left, right) + 1
+    
+    
+    
+'''
+Definition: A tree is height balance if difference of heights of left and right sub tree is at most 1 for 
+every node of the tree.
+
+Approach: Bottom-up approach to recursively check the height of left as well as right subtrees for every 
+node using DFS.
+
+* For ITERATIVE approach, refer this link: https://leetcode.com/problems/balanced-binary-tree/discuss/35708/VERY-SIMPLE-Python-solutions-(iterative-and-recursive)-both-beat-90
+
+* Refer NC solution video for detailed theoretical explanation on how this is O(n) slution
+'''

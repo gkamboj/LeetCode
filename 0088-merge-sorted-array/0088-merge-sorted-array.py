68795010249d7eae024327555c6c1e57c950623d
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-06-01 15:35:37 +0530

    Time: 47 ms (10.86%), Space: 16.5 MB (72.83%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.py
+++ 0088-merge-sorted-array/0088-merge-sorted-array.py
@@ -4,16 +4,17 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         i, j, k = m - 1, n - 1, m + n - 1
-        while j >= 0 and i >= 0:
-            if nums2[j] < nums1[i]:
+        while j >= 0:
+            if i >= 0 and nums2[j] < nums1[i]:
                 nums1[k] = nums1[i]
                 i -= 1
             else:
                 nums1[k] = nums2[j]
                 j -= 1
             k -= 1
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
-        
+    
+'''
+Approach: Take pointers from the end of expected length of both arrays (i.e. m-1 and n-1) and another point 
+from actual length of nums1 (m + n - 1) to store the numberss in empty spaces. Apply the merge sort algo and
+handle the case if first nums1 index gets over first (if while loop is on nums2 index) .
+'''

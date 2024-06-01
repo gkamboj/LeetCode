84f3f831b67af20998e389026b95822e473e3ae3
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-06-02 00:08:56 +0530

    Time: 45 ms (60.38%), Space: 17.4 MB (97.11%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.py
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.py
@@ -26,6 +26,11 @@ every node of the tree.
 Approach: Bottom-up approach to recursively check the height of left as well as right subtrees for every 
 node using DFS.
 
+* NOTE that unlike other submission (using global variable), where left and right are calculated with +1
+and return statement is just max(left, right) [without adding 1], here similar approach will fail and we 
+have to add 1 at end. This is because here we are modifying the return value in out method itself. Whereas 
+other submission was never modifying the return value, so it didn't matter at which step we are adding 1.
+
 * For ITERATIVE approach, refer this link: https://leetcode.com/problems/balanced-binary-tree/discuss/35708/VERY-SIMPLE-Python-solutions-(iterative-and-recursive)-both-beat-90
 
 * Refer NC solution video for detailed theoretical explanation on how this is O(n) slution

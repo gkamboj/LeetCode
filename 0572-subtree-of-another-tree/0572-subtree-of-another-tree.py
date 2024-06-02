429fd34dd79e8b325176b2a5c38f55ea0c2a6bfe
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-06-03 02:46:53 +0530

    Time: 68 ms (82.14%), Space: 16.5 MB (98.75%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.py
@@ -17,3 +17,10 @@ class Solution:
         if node1 and node2:
             return node1.val == node2.val and self.isSameTree(node1.left, node2.left) and self.isSameTree(node1.right, node2.right)
         return node1 is node2
+    
+'''
+Approach: Recursive - check if tree with current node as root is same as subRoot tree. If not, recursively check for left as well as right subtree.
+
+NOTE: Terminating condition returning False works because it's given that number of nodes will be at least 1 in both the nodes. If minimum nodes were
+0, then case with [],[] would have failed. 
+'''

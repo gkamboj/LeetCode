be234bade536b7dd760901b03ebd14a2fb78da44
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-06-02 21:11:31 +0530

    Time: 26 ms (96.50%), Space: 16.6 MB (9.10%) - LeetHub

--- 0100-same-tree/0100-same-tree.py
+++ 0100-same-tree/0100-same-tree.py
@@ -8,6 +8,14 @@ class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if p and q:
             return p.val == q.val and\
-        self.isSameTree(p.left, q.left) and\
-        self.isSameTree(p.right, q.right)
+                self.isSameTree(p.left, q.left) and\
+                self.isSameTree(p.right, q.right)
         return p is q
+    
+'''
+Approach: Recursive approach -  if both nodes are not null, check revursively for left as well as right sub trees; else return  True if both are null
+else False if either is null.
+
+NOTE: Even if inorder and preorder/postorder/levelorder can be used to uniquely identify a tree, that approach will not work here as there are test
+cases with duplicate notes. For eg., [1,1] and [1,null,1] will give same inorder and preorder values for both trees even though they are different.
+'''

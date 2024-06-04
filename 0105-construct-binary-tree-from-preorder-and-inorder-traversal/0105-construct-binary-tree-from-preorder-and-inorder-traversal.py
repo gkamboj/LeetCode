21f14893fc0ac8031fac6fcaeef0642e8d305795
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-06-04 23:31:54 +0530

    Time: 140 ms (24.51%), Space: 88.2 MB (38.62%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -17,4 +17,7 @@ class Solution:
         root.right = self.buildTree(preorder[inorderIndex:], inorder[inorderIndex + 1:])
         return root
     
-        
+'''
+Approach: Recursive - First value of preorder array is root, and values left of this root value in inorder array are nodes of left subtree, whereas 
+values on right are right subtree. Repeat this recursively to create the tree. For terminal/base condition, return when length of input arrays <= 1.
+'''

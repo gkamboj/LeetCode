8946f911f9f4963e955e2227fa5512e1a4f493c4
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-12-21 01:11:53 +0530

    Time: 0 ms (100.00%), Space: 18.9 MB (5.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,7 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        inDict = {}
-        for ind, val in enumerate(nums):
-            if (target - val) in inDict:
-                return [inDict[target - val], ind]
-            inDict[val] = ind
-            
-#Approach: One-pass hash table (3rd approach from solutions)
+        hm = {}
+        for ind, num in enumerate(nums):
+            if (target - num) in hm:
+                return [ind, hm[target - num]]
+            hm[num] = ind

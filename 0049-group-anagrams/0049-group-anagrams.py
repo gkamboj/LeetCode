cd98998cc40d6860818c6b647cbe55a30ed61132
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-12-21 23:21:02 +0530

    Time: 11 ms (80.85%), Space: 20.7 MB (41.33%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -0,0 +1,7 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        result = defaultdict(list)
+        for val in strs:
+            result[''.join(sorted(val))].append(val)
+        return list(result.values())
+        

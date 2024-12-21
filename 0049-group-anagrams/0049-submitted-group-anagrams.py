7b95d386fa88ccbe56b4e977dd5e3412fbafc902
Gaurav Kamboj <gauravkambojtu@gmail.com>
2024-12-21 23:22:36 +0530

    Time: 95 ms (38.19%), Space: 20.1 MB (66.92%)

--- 0049-group-anagrams/0049-submitted-group-anagrams.py
+++ 0049-group-anagrams/0049-submitted-group-anagrams.py
@@ -3,6 +3,7 @@ class Solution:
         result = defaultdict(list)
         for word in strs:
             result[''.join(sorted(word))].append(word)
-        return result.values()
+        return list(result.values())
     
 # Approach: Create a dictionary with key as sorted string of every element, so value for every key will be a list of anagram strings. 
+# Note: dict.values() returns view of list, not the list itself since recent Python versions. So, wrap it in list() before returning.

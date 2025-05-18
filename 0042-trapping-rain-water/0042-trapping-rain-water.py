5ffc0e97633653ae847bdfdbbc7c423eabf36ed0
Gaurav Kamboj <gauravkambojtu@gmail.com>
2025-05-18 22:50:31 +0530

    Time: 35 ms (13.17%), Space: 19.4 MB (39.91%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,16 +1,14 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        l, r = 0, len(height) - 1
-        ans = 0
-        l_m, r_m = height[l], height[r]
-        while l < r:
-            if l_m < r_m:
-                l+=1
-                l_m = max(height[l], l_m)
-                ans += l_m - height[l]
-            else:
-                r -= 1
-                r_m = max(r_m, height[r])
-                ans += r_m - height[r]
+        arr = [0 for i in range(len(height))]
+        ans, l_max, r_max = 0, height[0], height[-1]
+        for ind in range(len(height) - 1, -1, -1):
+            r_max = max(r_max, height[ind])
+            arr[ind] = r_max
+        for ind in range(len(height) - 1):
+            ans += max(min(l_max, arr[ind + 1]) - height[ind], 0)
+            l_max = max(l_max, height[ind])
         return ans
-                
+
+# Approach: Create an array to store maximum value from right at any index. Then traverse
+# and keep adding the water that can be stored due to current index.

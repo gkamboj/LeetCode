1b69c70898539b3ca5b1446a09667298124e8d01
Gaurav Kamboj <gauravkambojtu@gmail.com>
2025-05-19 02:00:20 +0530

    Tree traversals

--- PyDSCodes/ds/trees/implementations/traversals.py
+++ PyDSCodes/ds/trees/implementations/traversals.py
@@ -1,6 +1,7 @@
 from PyDSCodes.ds.trees.implementations.TreeNode import TreeNode
 
 
+# root - left - right
 def pre_order(root_node):
     stack, ans = [root_node], []
     while stack:
@@ -13,6 +14,7 @@ def pre_order(root_node):
     return ans
 
 
+# left - right - root
 def post_order(root_node):
     stack, ans = [], []
     curr = root_node
@@ -34,6 +36,20 @@ def post_order(root_node):
     return ans
 
 
+# Similar to pre-order, but do root-right-left. Reverse the result at end to get post-order.
+def post_order_2(root_node):
+    stack, ans = [root_node], []
+    while stack:
+        node = stack.pop()
+        ans.append(node.val)
+        if node.left:
+            stack.append(node.left)
+        if node.right:
+            stack.append(node.right)
+    return ans[::-1]
+
+
+# left - root - right
 def in_order(root_node):
     stack, ans = [], []
     curr = root_node
@@ -60,10 +76,47 @@ def level_order(root_node):
     return ans
 
 
+# LC-103
+def zig_zag_traversal(root_node):
+    queue, ans, level = [(root_node, 0)], [], 0
+    curr_level_nodes = []
+    while queue:
+        node, curr_level = queue.pop(0)
+        if curr_level == level:
+            curr_level_nodes.append(node.val)
+        else:
+            ans.append(curr_level_nodes[::-1] if level % 2 else curr_level_nodes)
+            curr_level_nodes = [node.val]
+            level = curr_level
+        if node.left:
+            queue.append((node.left, level + 1))
+        if node.right:
+            queue.append((node.right, level + 1))
+    ans.append(curr_level_nodes[::-1] if level % 2 else curr_level_nodes)
+    return ans
+
+
+# LC-103
+def zig_zag_traversal_v2(root_node):
+    queue, ans, reverse = [root_node], [], 1
+    while queue:
+        curr_level_nodes = queue
+        queue = []
+        for node in curr_level_nodes:
+            if node.left:
+                queue.append(node.left)
+            if node.right:
+                queue.append(node.right)
+        ans.append([node.val for node in curr_level_nodes[::reverse]])
+        reverse *= -1
+    return ans
+
+
 '''
 Tree with below level order traversal (with null nodes) is input. Check ./resources/example_1.png for visualisation of input tree.
 [1,2,3,4,5,6,7,8,null,null,9,10,null,null,null,null,null,null,null,11,12]
 '''
+
 root = TreeNode(1)
 root.left = TreeNode(2)
 root.right = TreeNode(3)
@@ -81,3 +134,10 @@ print(f'Level order traversal: {level_order(root)}')
 print(f'Inorder traversal: {in_order(root)}')
 print(f'Pre-order traversal: {pre_order(root)}')
 print(f'Post-order traversal: {post_order(root)}')
+print(f'Post-order 2 traversal: {post_order_2(root)}')
+print(f'Zig-zag traversal: {zig_zag_traversal(root)}')
+print(f'Zig-zag traversal v2: {zig_zag_traversal_v2(root)}')
+
+'''
+Universal idea of traversing binary trees (YET TO BE CHECKED): https://leetcode.com/problems/binary-tree-postorder-traversal/discuss/45559/My-Accepted-code-with-explaination.-Does-anyone-have-a-better-idea/45086
+'''

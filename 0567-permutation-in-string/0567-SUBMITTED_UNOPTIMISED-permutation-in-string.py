635ef74923d5fdebbd2a1b19e0f41ad83a5e84b3
Gaurav Kamboj <gauravkambojtu@gmail.com>
2025-06-16 01:06:07 +0530

    Time: 5 ms (99.81%), Space: 18 MB (18.57%)

--- 0567-permutation-in-string/0567-SUBMITTED_UNOPTIMISED-permutation-in-string.py
+++ 0567-permutation-in-string/0567-SUBMITTED_UNOPTIMISED-permutation-in-string.py
@@ -14,3 +14,7 @@ class Solution:
             arr2[ord(s2[i-n]) - ord('a')] -= 1
         return arr1 == arr2
 
+# Approach: Create 2 arrays - one each for s1 and s2 - to maintain the count of every character in
+# respective strings. Initialise both for n characters [n = len(s1)]. After that, start from index n
+# and keep moving the window towards the right. At every step, check if arr1 == arr2. 
+# Since equality check of array takes O(k), with ke being array size (26), total complexity is O(26n).

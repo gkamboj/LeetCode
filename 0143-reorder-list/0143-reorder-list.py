2ae6fe6a1e0788de904b2fc09dc31a4d35ffe193
Gaurav Kamboj <gauravkambojtu@gmail.com>
2025-06-16 02:51:04 +0530

    Time: 1 ms (60.15%), Space: 23.4 MB (38.46%) - LeetHub

--- 0143-reorder-list/0143-reorder-list.py
+++ 0143-reorder-list/0143-reorder-list.py
@@ -15,26 +15,17 @@ class Solution:
             prev = slow
             slow = slow.next
             fast = fast.next.next
-        mid = self.reverseList(prev.next)
         prev.next = None
-        n1, n2 = head, mid
+        n1, n2 = head, self.reverseList(slow)
         while n1 and n2:
-            curr = n1
+            curr = n2
             temp1 = n1.next
             temp2 = n2.next
             n1.next = n2
             n2.next = temp1
             n2 = temp2
             n1 = temp1
-        if n2:
-            curr.next.next = n2
-        # print("n1 is " + str(n1))
-        # print("n2 is " + str(n2))
-        # print("temp1 is " + str(temp1))
-        # print("temp2 is " + str(temp2))
-        # print("curr is " + str(curr))
-        # print("head is " + str(head))
-
+        curr.next = n2
 
 
     def reverseList(self, head):

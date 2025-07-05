043e260a3066229072dad8b0e9a85ab836f10dbb
Gaurav Kamboj <gauravkambojtu@gmail.com>
2025-07-06 03:06:15 +0530

    Time: 34 ms (80.81%), Space: 17.4 MB (31.82%)

--- 0138-copy-list-with-random-pointer/0138-WITH_SPACE-copy-list-with-random-pointer.py
+++ 0138-copy-list-with-random-pointer/0138-WITH_SPACE-copy-list-with-random-pointer.py
@@ -21,11 +21,14 @@ class Solution:
     
     
 '''
-Approach: Create a dictionary with given linkedlist's nodes as key. Iterate over the given list, and for every node as key, update the val, next and random for value corresponding to key. By 
-the end of while loop, every node in dictionary would be updated with corrrect values. Return value from map corresponding to head as key.
+Approach: Create a dictionary with given linkedlist's nodes as key. Iterate over the given list, and for every
+node as key, update the val, next and random for value corresponding to key. By  the end of while loop, every
+node in dictionary would be updated with corrrect values. Return value from map corresponding to head as key.
 '''
 
 '''
-Another approach: Without using dictionary, update the given original list such that for every node, insert its copy at next position. Fetch the new list from original list in next traversal. 
-Refer https://leetcode.com/problems/copy-list-with-random-pointer/discuss/43491/A-solution-with-constant-space-complexity-O(1)-and-linear-time-complexity-O(N) for detail.
+Another approach: Without using dictionary, update the given original list such that for every node, insert its
+copy at next position. Fetch the new list from original list in next traversal. 
+Refer https://leetcode.com/problems/copy-list-with-random-pointer/discuss/43491/A-solution-with-constant-space-complexity-O(1)-and-linear-time-complexity-O(N)
+for detail.
 '''

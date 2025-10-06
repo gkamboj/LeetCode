5835ca70362a465ec2468293d9f25282f83639bc
Gaurav Kamboj <gauravkambojtu@gmail.com>
2025-10-06 18:31:48 +0530

    Time: 6 ms (41.27%), Space: 17.8 MB (69.53%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,28 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        for r_ind, row in enumerate(board):
-            for c_ind, val in enumerate(row):
-                if val != '.' and not self.isValidCell(r_ind, c_ind, board):
+        rows_seen = set()
+        cols_seen = set()
+        boxes_seen = set()
+    
+        for r in range(9):
+            for c in range(9):
+                val = board[r][c]
+                if val==".":
+                    continue
+                box_key=(r//3,c//3,val)
+                row_key=(r,val)
+                col_key=(c,val)
+    
+                if row_key in rows_seen or col_key in cols_seen or box_key in boxes_seen:
                     return False
+                rows_seen.add(row_key)
+                cols_seen.add(col_key)
+                boxes_seen.add(box_key)
         return True
 
-    def isValidCell(self, x, y, board):
-        cell_val = board[x][y]
-        for ind, val in enumerate(board[x]):
-            if ind != y and val == cell_val:
-                print('hi1')
-                return False
-
-        for ind, row in enumerate(board):
-            if ind != x and row[y] == cell_val:
-                print('hi2')
-                return False
-
-        cell_row, cell_col = 3 * (x//3), 3 * (y//3)
-        for x1 in range(cell_row, cell_row + 3):
-            for y1 in range(cell_col, cell_col + 3):
-                if x1 != x and y1 != y and board[x1][y1] == cell_val:
-                    print('hi3')
-                    return False
-
-        return True
+# Interview - candidate solution

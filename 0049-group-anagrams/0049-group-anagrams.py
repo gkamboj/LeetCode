2723212a0f1fd656f0de1bd584999081dee0a6ce
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-01-17 00:58:02 +0530

    Time: 13 ms (54.67%), Space: 22 MB (33.58%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -2,12 +2,5 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         ana_map = defaultdict(list)
         for word in strs:
-            counter = self.counter(word)
-            ana_map[counter].append(word)
+            ana_map[''.join(sorted(word))].append(word)
         return list(ana_map.values())
-
-    def counter(self, word):
-        counter = [0 for i in range(26)]
-        for char in word:
-            counter[ord(char) - ord('a')] += 1
-        return tuple(counter)

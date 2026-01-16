a590f53e03e8b999dc5b369195d2d1b6f8b0e507
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-01-17 04:23:07 +0530

    Time: 19 ms (8.3%), Space: 19.7 MB (29.67%)

--- 0125-valid-palindrome/0125-SUBMITTED_SINGLE_POINTER_valid-palindrome.py
+++ 0125-valid-palindrome/0125-SUBMITTED_SINGLE_POINTER_valid-palindrome.py
@@ -3,6 +3,7 @@ class Solution:
         alpha_str = ''
         for char in s.lower():
             if ord('a') <= ord(char) <= ord('z') or ord('0') <= ord(char) <= ord('9'):
+            # if char.isalpha() or char.isnumeric():
                 alpha_str += char
         
         for ind in range(len(alpha_str) - 1//2):

ace8352409fe30f8bca7ab7c458440a405f8a702
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-01-18 01:35:41 +0530

    Time: 39 ms (80.27%), Space: 28.7 MB (14.32%)

--- 0121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 0121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,14 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        if len(prices) == 0: return 0
-        
-        minPriceYet, ans = prices[0], 0
+        ans, curr_min = 0, 10**4 + 1
         for price in prices:
-            minPriceYet = min(minPriceYet, price)
-            currProfit = price - minPriceYet
-            ans = max(currProfit, ans)
+            curr_min = min(price, curr_min)
+            ans = max(ans, price - curr_min)
         return ans
     
-#Approach: For every price, update minimum price till now (if it's lesser than min price) and find profit. Update global profit accordingly.
+#Approach: For every price, update the minimum price till now (if it's lesser than the min price) and find profit. Update global profit accordingly.
 
 #See other submissions for other approaches

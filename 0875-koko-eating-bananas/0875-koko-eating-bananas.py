7ed0ee0d3bd253ff2224769af1a583bd7bda2ade
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-01-28 01:29:24 +0530

    Time: 160 ms (61.02%), Space: 20.8 MB (5.78%) - LeetHub

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.py
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -1,9 +1,7 @@
-import math
-
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
-        low = 1 # minimum speed
-        high = ans = max(piles)
+        low = 1
+        ans = high = max(piles)
         while low <= high:
             mid = low + (high - low) // 2
             hours = self.eatingHours(piles, mid)
@@ -13,17 +11,10 @@ class Solution:
                 high = mid - 1
                 ans = min(ans, mid)
         return ans
-    
-    
-    def eatingHours(self, piles, speed):
-        val = 0
-        for pile in piles:
-            val += -(pile // -speed)
-        return val
 
-    def eatingHoursV2(self, piles, speed):
+
+    def eatingHours(self, piles, speed):
         hours = 0
         for count in piles:
-            hours += math.ceil(count / speed)
+            hours += -(-count//speed)
         return hours
-

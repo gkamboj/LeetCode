110060c11fcab381e630375221fd5a32f98bb772
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-01-28 00:14:56 +0530

    Time: 529 ms (11.32%), Space: 30.5 MB (57.70%)

--- 0287-find-the-duplicate-number/0287-2-pointers-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-2-pointers-find-the-duplicate-number.py
@@ -1,19 +1,21 @@
 class Solution:
     def findDuplicate(self, nums: List[int]) -> int:
-        slow, fast = nums[0], nums[nums[0]]
-        while slow != fast:
+        slow = fast = 0 #both pointers start from zero index
+        while nums[slow] != nums[nums[fast]]:
             slow = nums[slow]
             fast = nums[nums[fast]]
-        slow = 0
+        
+        slow, fast = 0, nums[nums[fast]] #move back start to head (zero index), keep fast at collision point of start and fast
         while slow != fast:
             slow = nums[slow]
             fast = nums[fast]
+        
         return slow
     
 '''
-Approach: 2-pointers using Floyd's cycle/Hare-Tortoise algo. Refer following:
+Approach: 2-pointers using Floyd's cycle/Hare-Tortoise algorithm. Refer following:
 1. https://keithschwarz.com/interesting/code/?dir=find-duplicate
 2. https://leetcode.com/problems/find-the-duplicate-number/discuss/72846/My-easy-understood-solution-with-O(n)-time-and-O(1)-space-without-modifying-the-array.-With-clear-explanation.
 
-See video from 9:00 for proof of Floyd's algo: https://www.youtube.com/watch?v=wjYnzkAhcNk
+See video from 9:00 for proof of Floyd's algorithm: https://www.youtube.com/watch?v=wjYnzkAhcNk or check the LC-142 solution with PDF.
 '''

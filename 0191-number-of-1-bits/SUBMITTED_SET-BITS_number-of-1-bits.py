dbe3e89a9805ddd97ff49eca234981c557225775
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-02-03 02:15:44 +0530

    Time: 0 ms (100%), Space: 19.4 MB (11.6%)

---
+++ 0191-number-of-1-bits/SUBMITTED_SET-BITS_number-of-1-bits.py
@@ -0,0 +1,17 @@
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n != 0:
+            count += 1
+            n = n & (n - 1)
+        return count
+
+# Approach: n & (n - 1) unsets the rightmost set bet of n. So, keep unsetting rightmost bits till every bit
+# unsets (and hence n becomes zero). TC -> O(no. of set bits)
+
+# See https://leetcode.com/problems/number-of-1-bits/discuss/55099/Simple-Java-Solution-Bit-Shifting to understand
+# difference if working in Java
+
+# See other submission of O(number of bits in N) approach
+
+# Check LC-338 to answer Follow-up question

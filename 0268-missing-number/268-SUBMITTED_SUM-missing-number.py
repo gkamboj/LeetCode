df421b4cfcf7ebf1a5d1f1c2a7dba758fe222109
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-02-03 02:58:19 +0530

    Time: 227 ms (39.24%), Space: 15.1 MB (98.28%)

--- 0268-missing-number/268-SUBMITTED_SUM-missing-number.py
+++ 0268-missing-number/268-SUBMITTED_SUM-missing-number.py
@@ -4,3 +4,5 @@ class Solution:
         for ind in range(len(nums)):
             ans = ans + (ind + 1) - nums[ind]
         return ans
+
+# Approach: Same as subtracting array sum from sum of first n numbers, but doing in loop to avoid overflow that can happen in case of high n.

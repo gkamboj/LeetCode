dc1ff87b1addcb644a099c990b8e3c82dae055e1
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-02-07 01:01:14 +0530

    Time: 0 ms (100%), Space: 19.5 MB (17.76%) - LeetHub

---
+++ 0043-multiply-strings/0043-multiply-strings.py
@@ -0,0 +1,16 @@
+class Solution:
+    def multiply(self, num1: str, num2: str) -> str:
+        if num2 == "0" or num1 == "0":
+            return "0"
+        
+        n1 = max(int(num1), int(num2))
+        n2 = min(int(num1), int(num2))
+
+        ans = 0
+        while n2:
+            if n2 & 1:
+                ans += n1
+            n2 >>= 1
+            n1 *= 2
+        
+        return str(ans)

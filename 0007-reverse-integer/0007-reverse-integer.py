2d10f33800d5453edba8a559f09cbfc3b72f515d
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-02-17 01:02:46 +0530

    Time: 45 ms (49.38%), Space: 19.3 MB (51.72%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,12 +1,16 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        ans = 0
+        ans, max_val = 0, 2 ** 31
         neg = -1 if x < 0 else 1
         x = abs(x)
         while x:
             rem = x % 10
             x //= 10
-            ans  = (ans * 10) + rem
-            if ans >= 2 ** 31:
+            if (ans > max_val / 10) or (ans == max_val // 10 and rem > max_val % 10):
                 return 0
+            ans  = (ans * 10) + rem
+            
         return ans * neg
+
+# Approach: Mathematical algo to revser number. To handle 32 bit constraint, check that updated ans should not be more than
+# max_val (2^31) before actually making the update.

ff25bdc286423216d33d3fec462d33300bfe1ebd
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-02-23 00:47:33 +0530

    0253-SUBMITTED_HEAP-meeting-rooms-ii

---
+++ 0253-meeting-rooms-ii/0253-SUBMITTED_HEAP-meeting-rooms-ii.py
@@ -0,0 +1,19 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+import heapq
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        heap = []
+        intervals.sort(key = lambda x:x.start)
+        for val in intervals:
+            if heap and heap[0] <= val.start:
+                heapq.heappop(heap)
+            heapq.heappush(heap, val.end)
+        return len(heap)

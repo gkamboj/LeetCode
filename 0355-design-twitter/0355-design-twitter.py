69b7b84d9189d683d689751510d6b187b530fad5
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-02-25 23:46:01 +0530

    Time: 11 ms (75.22%), Space: 27.6 MB (72.54%) - LeetHub

--- 0355-design-twitter/0355-design-twitter.py
+++ 0355-design-twitter/0355-design-twitter.py
@@ -9,18 +9,31 @@ class Twitter:
 
     def postTweet(self, userId: int, tweetId: int) -> None:
         self.count += 1
-        self.tweets[userId].append([-self.count, tweetId])
+        user_tweets = self.tweets[userId]
+        user_tweets.append([-self.count, tweetId])
+        if len(user_tweets) > 10:
+            user_tweets.pop(0)
 
     def getNewsFeed(self, userId: int) -> List[int]:
         users = {userId}.union(self.following[userId])
         feed_tweets = []
-        # print(users)
         for user in users:
-            feed_tweets += list(self.tweets[user])
-        h.heapify(feed_tweets)
+            user_tweets = self.tweets[user]
+            if user_tweets:
+                count, tweet_id = user_tweets[-1]
+                ind = len(user_tweets) - 1
+                if len(feed_tweets) == 10:
+                    h.heappop(feed_tweets)
+                h.heappush(feed_tweets, [count, tweet_id, user, ind - 1])
+
         feed = []
         while len(feed) != 10 and feed_tweets:
-            feed.append(h.heappop(feed_tweets)[1])
+            _, tweet_id, user, ind = h.heappop(feed_tweets)
+            feed.append(tweet_id)
+            if ind >= 0:
+                count, tweet_id = self.tweets[user][ind]
+                h.heappush(feed_tweets, [count, tweet_id, user, ind - 1])
+
         return feed
 
 
@@ -37,3 +50,10 @@ class Twitter:
 # param_2 = obj.getNewsFeed(userId)
 # obj.follow(followerId,followeeId)
 # obj.unfollow(followerId,followeeId)
+
+# Approach: Everything other than getNewsFeed and postTweet is self-explanatory.
+# For postTweet, append count as well as tweet_id to tweets array maintained for every user. count is needed to know the insertion order.
+# For getNewsFeed, we have k ordered arrays (ordered by tweet posting order). Take the most recently posted tweet from each of them and
+# create max heap (using -1 * count as key). Store user id and index of current tweet also in heap to add subsequent value if current tweet is
+# removed from heap.
+# Refer NC video solution for detail: https://youtu.be/pNichitDD2E

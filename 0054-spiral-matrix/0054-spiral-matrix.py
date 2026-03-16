3f913f1aebfb56dcf9df81446f7007d7b6167320
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-17 01:25:18 +0530

    Time: 0 ms (100%), Space: 19.2 MB (89.77%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,28 +1,30 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        res = []
-        for ind in range(math.ceil(min(len(matrix), len(matrix[0])) / 2)):
-            res += self.outerSpiral(matrix, ind)
-        return res
+        result, loopEnd = [], ((min(len(matrix), len(matrix[0])) - 1) // 2) + 1
+        for i in range(loopEnd):
+            result += self.outerSpiral(matrix, i)
+        return result
+
 
     def outerSpiral(self, matrix, start):
-        r_end, c_end = len(matrix) - start, len(matrix[0]) - start
-        if c_end - start == 1:
-            return [row[start] for row in matrix[start : r_end]]
+        spiral, colEnd, rowEnd = [], len(matrix[0]) - start, len(matrix) - start
+        if colEnd - start == 1:
+            return [row[start] for row in matrix[start:rowEnd]]
         
-        res = matrix[start][start : c_end]
+        spiral.extend(matrix[start][start:colEnd])
         
-        ind = start + 1
-        while ind < r_end - 1:
-            res.append(matrix[ind][-start-1])
-            ind += 1
+        row = start + 1
+        while row < rowEnd - 1:
+            spiral.append(matrix[row][-start-1])
+            row += 1
         
-        if r_end - 1 != start:
-            res.extend(matrix[-start-1][start : c_end][::-1])
+        if start != rowEnd - 1:
+            spiral.extend(matrix[-start-1][start:colEnd][::-1])
         
-        ind = r_end - 2
-        while ind > start:
-            res.append(matrix[ind][start])
-            ind -= 1
+        row = rowEnd - 2
+        while row > start:
+            spiral.append(matrix[row][start])
+            row -= 1
         
-        return res
+        return spiral
+        

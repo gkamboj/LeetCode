030e41b77ac42473436646cb319eb13cc2432483
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-17 23:43:28 +0530

    Time: 42 ms (61.68%), Space: 29.4 MB (37.13%)

--- 0041-first-missing-positive/0041-n-SPACE-first-missing-positive.py
+++ 0041-first-missing-positive/0041-n-SPACE-first-missing-positive.py
@@ -1,24 +1,24 @@
 class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
-        min_val = max(nums)
-        if min_val <= 0:
+        if (high := max(nums)) <= 0:
             return 1
-        for val in nums:
-            if val > 0 and val < min_val:
-                min_val = val
-        if min_val != 1:
-            return 1
-        helper = [0 for i in range(len(nums))]
-        for val in nums:
-            if val > 0 and val <= len(nums):
-                helper[val - 1] = 1
-        for ind, val in enumerate(helper):
-            if val == 0:
-                return ind + 1
-        return len(nums) + 1
+        
+        n = min(len(nums), high) + 1
+        arr = [1 for i in range(n)]
+        
+        for num in nums:
+            if 0 < num < n:
+                arr[num] = 0
+        
+        for ind, num in enumerate(arr):
+            if ind and num:
+                return ind
+        
+        return n
 
 # Approach: O(n) time with O(n) space.
-# (1) Check for cases when all numbers are negative or smallest non positive != 1. Return 1 for these cases.
-# (2) For remaining cases, create a helper array such that values at indexes present in original array are 1, else zero.
-# Return first non-zero index.
-# Return len(nums) + 1 if all values in helper are 1 (to handle cases when input is of all numbers from 1 to n)
+# (1) If all numbers are non-positive, directly return 1.
+# (2) For the remaining cases, create a helper array of a length of at least the length of nums and the maximum of nums,
+# with every member set as 1. For every number of nums, set the value at the corresponding index to 0.
+# Return the first index with a non-zero value.
+# Return len(nums) + 1 if all values in helper are 0 (to handle cases when input is of all numbers from 1 to n)

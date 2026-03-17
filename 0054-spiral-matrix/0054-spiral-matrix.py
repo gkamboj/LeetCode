b0d0f9596074d090620a76f31e31e5ccf7442ecf
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-18 01:08:49 +0530

    Time: 0 ms (100%), Space: 19.5 MB (6.12%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.py
+++ 0054-spiral-matrix/0054-spiral-matrix.py
@@ -1,32 +1,31 @@
 class Solution:
     def spiralOrder(self, matrix: List[List[int]]) -> List[int]:
-        result, loopEnd = [], ((min(len(matrix), len(matrix[0])) - 1) // 2) + 1
-        for i in range(loopEnd):
-            result += self.outerSpiral(matrix, i)
-        return result
+        left, right = 0, len(matrix[0])
+        top, bottom = 0, len(matrix)
+        ans = []
 
+        while left < right and top < bottom:
+            for ind in range(left, right):
+                ans.append(matrix[top][ind])
+            top += 1
+            
+            for ind in range(top, bottom):
+                ans.append(matrix[ind][right - 1])
+            right -= 1
 
-    def outerSpiral(self, matrix, start):
-        spiral, colEnd, rowEnd = [], len(matrix[0]) - start, len(matrix) - start
-        if colEnd - start == 1:
-            return [row[start] for row in matrix[start:rowEnd]]
-        elif rowEnd - start == 1:
-            return matrix[start][start:colEnd]
+            if right > left and bottom > top:
+                for ind in range(right - 1, left - 1, -1):
+                    ans.append(matrix[bottom - 1][ind])
+                bottom -= 1
+            
+                for ind in range(bottom - 1, top - 1, -1):
+                    ans.append(matrix[ind][left])
+                left += 1
         
-        spiral.extend(matrix[start][start:colEnd])
-        
-        row = start + 1
-        while row < rowEnd - 1:
-            spiral.append(matrix[row][-start-1])
-            row += 1
-        
-        if start != rowEnd - 1:
-            spiral.extend(matrix[-start-1][start:colEnd][::-1])
-        
-        row = rowEnd - 2
-        while row > start:
-            spiral.append(matrix[row][start])
-            row -= 1
-        
-        return spiral
-        
+        return ans
+
+'''
+Approach: Move right → down → left → up, repeatedly, moving inward layer by layer.
+Take care of end indices to avoid index out of bound and missed numbers.
+Refer NC iterative solution for more detail.
+'''

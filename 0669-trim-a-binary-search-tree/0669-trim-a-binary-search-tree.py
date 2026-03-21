e32367a85c1b7f243f40250abd44cffcb466d6da
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-22 00:10:19 +0530

    Time: 3 ms (16.33%), Space: 22.5 MB (28.33%) - LeetHub

---
+++ 0669-trim-a-binary-search-tree/0669-trim-a-binary-search-tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def trimBST(self, root: Optional[TreeNode], low: int, high: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+            
+        if low <= root.val <= high:
+            root.left = self.trimBST(root.left, low, high)
+            root.right = self.trimBST(root.right, low, high)
+            return root
+        elif root.val < low:
+            return self.trimBST(root.right, low, high)
+        else:
+            return self.trimBST(root.left ,low, high)
+        
+        return root
+        
+'''
+Approach: 
+- If root value is in range, recursively fix left and right sub trees before returning root.
+- If root value is l.t. low, recursively call for right child as whole of left sub tree is out of range.
+- For remaining case (root val g.t. high), call left child.
+'''

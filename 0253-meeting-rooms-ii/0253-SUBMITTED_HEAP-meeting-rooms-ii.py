999830cf52afd9b8107f94a1e648e06796179569
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-22 01:20:06 +0530

    Update 0253-SUBMITTED_HEAP-meeting-rooms-ii.py

--- 0253-meeting-rooms-ii/0253-SUBMITTED_HEAP-meeting-rooms-ii.py
+++ 0253-meeting-rooms-ii/0253-SUBMITTED_HEAP-meeting-rooms-ii.py
@@ -17,3 +17,11 @@ class Solution:
                 heapq.heappop(heap)
             heapq.heappush(heap, val.end)
         return len(heap)
+
+'''
+Approach: 
+- Heap stores the end times of meetings currently occupying rooms.
+- Smallest end time is always at the top, representing the room that frees up the earliest.
+- If the earliest-ending meeting finishes before the current one starts, we can reuse that room by popping the current minimum from heap.
+- Else, we must allocate a new room.
+'''

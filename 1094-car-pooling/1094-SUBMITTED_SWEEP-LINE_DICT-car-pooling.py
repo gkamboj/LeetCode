f3d42445616389b935fb4f0f2a954bffa322e133
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-22 01:34:33 +0530

    Time: 3 ms (64.81%), Space: 19.6 MB (76%)

---
+++ 1094-car-pooling/1094-SUBMITTED_SWEEP-LINE_DICT-car-pooling.py
@@ -0,0 +1,23 @@
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        events = defaultdict(int)
+        for (num, frm, to) in trips:
+            events[frm] += num
+            events[to]-= num
+        
+        curr = 0
+        for event in sorted(events.keys()):
+            curr += events[event]
+            if curr > capacity:
+                return False
+        
+        return True
+
+'''
+Approach: Same as sweep line array, just using a dictionary instead of an array.
+Using a dictionary reduces space complexity if not all times are covered from 1 to 1000.
+
+Consider every pickup and drop as an event: with pickup time adding the passenger at that point of time,
+and drop time subtracting those passengers. Now, if at any time the number of passengers surpasses capacity, polling
+can't be done. If there's no such time, pooling is possible.
+'''

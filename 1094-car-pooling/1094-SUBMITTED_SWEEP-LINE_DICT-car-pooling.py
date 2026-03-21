b21d5250b37e7658933d83d98d84a38ed5482fb3
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-22 01:35:22 +0530

    Time: 3 ms (64.81%), Space: 19.6 MB (76%)

--- 1094-car-pooling/1094-SUBMITTED_SWEEP-LINE_DICT-car-pooling.py
+++ 1094-car-pooling/1094-SUBMITTED_SWEEP-LINE_DICT-car-pooling.py
@@ -15,7 +15,7 @@ class Solution:
 
 '''
 Approach: Same as sweep line array, just using a dictionary instead of an array.
-Using a dictionary reduces space complexity if not all times are covered from 1 to 1000.
+Using a dictionary reduces space complexity if not all times are covered from 1 to 1000, but needs extra sorting time.
 
 Consider every pickup and drop as an event: with pickup time adding the passenger at that point of time,
 and drop time subtracting those passengers. Now, if at any time the number of passengers surpasses capacity, polling

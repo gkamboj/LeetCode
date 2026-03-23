984e0077509bd2596e7abd0e0ec0c7425cebf3b1
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-24 00:44:03 +0530

    Time: 3 ms (64.81%), Space: 19.8 MB (23.08%)

--- 1094-car-pooling/1094-SUBMITTED_HEAP-car-pooling.py
+++ 1094-car-pooling/1094-SUBMITTED_HEAP-car-pooling.py
@@ -2,11 +2,10 @@ class Solution:
     def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
         trips.sort(key = lambda x:x[1])
         heap, tot = [], 0
-        print(trips)
+
         for (num, frm, to) in trips:
             while heap and heap[0][0] <= frm:
                 _, count = heapq.heappop(heap)
-                print(count)
                 tot -= count
             heapq.heappush(heap, (to, num))
             tot += num
@@ -14,3 +13,10 @@ class Solution:
                 return False
         
         return True
+
+'''
+Approach: Similar to LC-253 (Meeting Rooms II), maintain a min heap to the current minimum end time as well as the capacity
+of the trip with that time. For every new trip, if the start time is after the minimum end time of the heap, remove it from the heap
+and subtract the capacity from the total till now.
+If the total surpasses the input max capacity at any point, return False.
+'''

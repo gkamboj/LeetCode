d0cf51447f7a88c20194816277a35456b32e5dad
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-25 01:26:20 +0530

    Time: 3 ms (94.33%), Space: 20.2 MB (96.16%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -5,11 +5,17 @@ class Solution:
             while stack and ast < 0 < stack[-1]:
                 if stack[-1] < -ast:
                     stack.pop()
+                    continue # if there is collision, we should continue to avoid breaking in this interation
                 elif stack[-1] == -ast:
                     stack.pop()
-                    break
-                else:
-                    break
-            else:
+                break
+            else: # else will always be called after while if while loop ends naturally (that is without break)
                 stack.append(ast)
         return stack
+
+'''
+Approach: Use a stack to keep surviving asteroids. For each incoming asteroid, 
+repeatedly check collisions with the top of the stack (only when top is positive and 
+incoming is negative), popping smaller ones. If the incoming asteroid survives all 
+collisions, append it to the stack.
+'''

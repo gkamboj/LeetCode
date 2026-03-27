a7c89cb5b715ed974935cc09eec1271411334589
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-27 14:20:51 +0530

    Time: 0 ms (100%), Space: 19.4 MB (41.15%)

--- 0371-sum-of-two-integers/0371-SUBMITTED_sum-of-two-integers.py
+++ 0371-sum-of-two-integers/0371-SUBMITTED_sum-of-two-integers.py
@@ -2,32 +2,33 @@ class Solution:
     def getSum(self, a: int, b: int) -> int:
         ans = carry = 0
         neg = 1
+        
         if a < 0 and b < 0:
             a = abs(a)
             b = abs(b)
             neg = -1
         elif a < 0 or b < 0:
-            if a < 0 and abs(a) > b:
+            if (a < 0 and abs(a) > b) or (b < 0 and abs(b) > a):
                 neg = -1
-                b = -1 * b
-                a = abs(a)
-            elif b < 0 and abs(b) > a:
-                neg = -1
-                b = abs(b)
-                a = -1 * a
+                b = -b
+                a = -a
 
         for i in range(11):
             a_bit, b_bit = (a >> i) & 1, (b >> i) & 1
             ans |= (a_bit ^ b_bit ^ carry) << i
             carry = (a_bit & b_bit) | (a_bit & carry) | (b_bit & carry)
+        
         return ans * neg
 
-# Approach: Addition of two integers can be thought of adding them bit by bit and can be sub divided into two operations -
-# adding respective bits & considering carry over part.
-# Addition of respective bits can be calculated from XOR = a_bit ^ b_bit ^ carry. 
-# To find carry: carry cann be 1 or 0: 1 if atleast two out of a_bit, b_bit and previous carry are 1.
+'''
+Approach:
+The addition of two integers can be thought of as adding them bit by bit, and can be subdivided into two operations:
+- Adding respective bits & considering carry over part.
+- Addition of respective bits can be calculated from XOR = a_bit ^ b_bit ^ carry. 
+To find carry: carry can be 1 or 0: 1 if at least two out of a_bit, b_bit, and previous carry are 1.
 
-# Note: We are handling cases with negative answers explicitly because unlike Java, there is no cyclic overflow in Python. 
+Note: We are handling cases with negative answers explicitly because unlike Java, there is no cyclic overflow in Python. 
 
-# Note: Since we are comparing a and b with >/< signs, it's equivalent to using +/- sign which is what we really have to avoid.
-# So, this solution may be considered invalid. Prefer writing in Java for this.
+Note: Since we are comparing a and b with >/< signs, it's equivalent to using +/- sign which is what we really have to avoid.
+So, this solution may be considered invalid. Prefer writing in Java for this.
+'''

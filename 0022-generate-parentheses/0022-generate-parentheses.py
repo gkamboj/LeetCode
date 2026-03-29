fd5dde9a84a3004fe5fbd1853870874c95928707
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-03-30 01:33:38 +0530

    Time: 3 ms (29.61%), Space: 19.3 MB (84.51%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,21 +1,15 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
-        stack, ans = [], []
+        result = []
+        self.helper(result, n, n, '')
+        return result
 
-        def helper(open, close):
-            if len(stack) == 2*n:
-                ans.append(''.join(stack))
-            
-            if open < n:
-                stack.append('(')
-                helper(open + 1, close)
-                stack.pop()
-            
-            if close < open:
-                stack.append(')')
-                helper(open, close + 1)
-                stack.pop()
-            
-        helper(0, 0)
-        return ans
-            
+    def helper(self, result, start, end, val):
+        if not end:
+            result.append(val)
+        elif not start:
+            self.helper(result, start, end - 1, val + ')')
+        else:
+            self.helper(result, start - 1, end, val + '(')
+            if start < end:
+                self.helper(result, start, end - 1, val + ')')

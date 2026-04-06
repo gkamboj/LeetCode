2b723c7c7d17b095bb4edc936e3e520009ed9f5f
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-04-07 01:18:16 +0530

    Time: 3 ms (96.92%), Space: 19.6 MB (65.67%) - LeetHub

---
+++ 0720-longest-word-in-dictionary/0720-longest-word-in-dictionary.py
@@ -0,0 +1,21 @@
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        ans = ''
+        words_set, pre = set(words), set()
+        
+        for word in words:
+            if len(word) > len(ans) or (len(word) == len(ans) and word < ans):
+                if word in pre:
+                    ans = word
+                    continue
+                curr = ''
+                for char in word:
+                    curr += char
+                    if curr in words_set:
+                        pre.add(curr)
+                    else:
+                        break
+                else:
+                    ans = word
+        
+        return ans

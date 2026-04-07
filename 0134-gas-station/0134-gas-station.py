b4e32a4c0a046608b906a1834f3fdb85be2658b9
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-04-08 00:16:39 +0530

    Time: 33 ms (32.32%), Space: 26 MB (77.72%)

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,6 +1,6 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        tot = curr = ans = 0
+        tot = curr = ans = 0 # curr act as current remaining tank capacity 
         for ind in range(len(gas)):
             diff = gas[ind] - cost[ind]
             tot += diff
@@ -9,4 +9,10 @@ class Solution:
                 curr = 0
                 ans = ind + 1
 
-        return ans if ans < len(gas) and tot >= 0 else -1
+        return ans if tot >= 0 else -1
+
+'''
+Approach: Keep maintaining the total tank capacity since the start, and the tank capacity since the last start.
+Update the current capacity to zero whenever it becomes negative and `ans` to the next index. If the total capacity
+at the end is positive, it means the current answer fulfills the requirement.
+'''

803a65a91e30207976ecb740dcf2f26aa1960f86
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-04-11 12:19:53 +0530

    Time: 9 ms (85.7%), Space: 22.8 MB (86.94%) - LeetHub

---
+++ 0386-lexicographical-numbers/0386-lexicographical-numbers.py
@@ -0,0 +1,23 @@
+class Solution: 
+    def lexicalOrder(self, n: int) -> List[int]:
+        curr, result = 1, []
+        for _ in range(n):
+            result.append(curr)
+            
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+                while curr % 10 == 9 or curr >= n:
+                    curr //= 10
+                curr += 1
+        
+        return result
+
+'''
+Approach: Official LC iterative solution.
+- Iterate a loop of size n
+- Multiply the curr with 10 if it is within limit
+- Else, keep dividing curr by 10 if it ends with 9 (as next after 119 should 12, not 120)
+or out of the limit.
+- Increment curr with 1 to cotinue the cycle
+'''

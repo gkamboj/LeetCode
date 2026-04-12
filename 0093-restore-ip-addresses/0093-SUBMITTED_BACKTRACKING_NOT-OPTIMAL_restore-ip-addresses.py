98c4b0f88b68a77ae53832040b84bc3a87861e0d
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-04-12 17:27:17 +0530

    Time: 4 ms (24.88%), Space: 19.4 MB (41.59%)

--- 0093-restore-ip-addresses/0093-SUBMITTED_BACKTRACKING_NOT-OPTIMAL_restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-SUBMITTED_BACKTRACKING_NOT-OPTIMAL_restore-ip-addresses.py
@@ -17,4 +17,9 @@ class Solution:
         
         if len(s) > 2 and 100 <= int(val := s[:3]) <= 255:
             self.helper(s[3:], curr + [val], result)
-        
+
+'''
+Approach: Backtracking
+Backtrack with a start pointer, trying segment lengths 1–3 at each step. Prune if the remaining characters don't fit in the remaining parts.
+Break (not continue) on leading zero or segment > "255" since longer lengths will also be invalid.
+'''

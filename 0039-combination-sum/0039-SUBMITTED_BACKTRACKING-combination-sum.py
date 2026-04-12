05058e02b6286b8cbed1fb581b014d6824427b8d
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-04-13 00:31:20 +0530

    Time: 0 ms (100%), Space: 19.2 MB (99.61%)

--- 0039-combination-sum/0039-SUBMITTED_BACKTRACKING-combination-sum.py
+++ 0039-combination-sum/0039-SUBMITTED_BACKTRACKING-combination-sum.py
@@ -7,7 +7,7 @@ class Solution:
 
     def helper(self, candidates, combo, start, target, result):
         if not target:
-            result.append(combo[:])
+            result.append(combo[:]) # use `combo[:]` or `list(combo)`, and not `combo` as `combo` is a pointer to a list which is becoming empty at the end. Using it directly will add an empty list only.
             return
         
         for ind in range(start, len(candidates)):
@@ -21,7 +21,7 @@ class Solution:
 Approach: Backtracking + DFS
 - Sort the candidates to enable early pruning.
 - Use DFS/backtracking with parameters: combo, start, and target.
-- At each step, try all candidates from start onward as elements reuse is allowed.
-- If candidate > target, break early (because array is sorted).
-- When target == 0, add a copy of current combo to result.
+- At each step, try all candidates from start onward as element reuse is allowed.
+- If candidate > target, break early (because the array is sorted).
+- When target == 0, add a copy of the current combo to the result.
 '''

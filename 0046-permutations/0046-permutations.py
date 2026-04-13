a5c43f389a8f8fbf6690f306db42610f20e95b02
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-04-14 01:45:53 +0530

    Time: 1 ms (43.95%), Space: 19.5 MB (71.36%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,17 +1,25 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        ans = [[nums[0]]]
+        ans = [[]]
         
-        for ind in range(1, len(nums)):
+        for num in nums:
             temp = []
             for combo in ans:
-                temp += self.helper(combo, nums[ind])
-            ans = list(temp)
+                temp.extend(self.expand_permutations(combo, num))
+            ans = temp
         
         return ans
 
-    def helper(self, arr, num):
+    def expand_permutations(self, arr, num):
         result = []
         for i in range(len(arr) + 1):
             result.append(arr[:i] + [num] + arr[i:])
         return result
+
+'''
+Approach: Iterative BFS
+- Build permutations iteratively by inserting each new number into all positions of existing permutations.  
+- Start with the first element, then for every next number, generate new lists by placing it at every index.  
+- Use a temporary list to store newly formed permutations at each step.  
+- Update the answer list after processing each element.
+'''

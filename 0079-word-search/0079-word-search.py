33f4d32508d66bcfda084ee4b45c390183e17ab2
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-04-18 03:11:39 +0530

    Time: 4630 ms (23.46%), Space: 19.1 MB (98.79%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,35 +2,31 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if self.helper(board, {}, i, j, word):
+                if self.helper(board, i, j, {}, word):
                     return True
         return False
 
-    def helper(self, board, covered, i, j, word):
+    def helper(self, board, i, j, covered, word):
         if not word:
             return True
-
-        if i < 0 or j < 0 or i >= len(board) or j >= len(board[0]):
-            return False
-
-        if (i, j) in covered:
-            return False
-
-        if board[i][j] != word[0]:
+        elif (
+            not (0 <= i < len(board) and 0 <= j < len(board[0])) or 
+            (i, j) in covered or 
+            word[0] != board[i][j]
+        ):
             return False
 
         covered[(i, j)] = 1
-        word = word[1:]
+        ans = (
+            self.helper(board, i - 1, j, covered, word[1:]) or
+            self.helper(board, i + 1, j, covered, word[1:]) or
+            self.helper(board, i, j - 1, covered, word[1:]) or
+            self.helper(board, i, j + 1, covered, word[1:])
+        )
+        del covered[(i, j)]
+        
+        return ans
 
-        v1 = self.helper(board, covered, i + 1, j, word)
-        v2 = self.helper(board, covered, i - 1, j, word)
-        v3 = self.helper(board, covered, i, j + 1, word)
-        v4 = self.helper(board, covered, i, j - 1, word)
-
-        covered.pop((i, j)) 
-
-        return v1 or v2 or v3 or v4
-
-'''
-Approach: 
-'''
+# A B C E
+# S F E S
+# A D E E

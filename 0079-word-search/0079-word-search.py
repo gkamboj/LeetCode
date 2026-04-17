bcb599aa72fa4227ca1abb3334d12a16efc19047
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-04-18 03:22:11 +0530

    Time: 3897 ms (50.83%), Space: 19.3 MB (95.69%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,31 +2,27 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if self.helper(board, i, j, {}, word):
+                if self.helper(board, i, j, word):
                     return True
         return False
 
-    def helper(self, board, i, j, covered, word):
+    def helper(self, board, i, j, word):
         if not word:
             return True
         elif (
             not (0 <= i < len(board) and 0 <= j < len(board[0])) or 
-            (i, j) in covered or 
-            word[0] != board[i][j]
+            board[i][j] == '.' or 
+            board[i][j] != word[0]
         ):
             return False
 
-        covered[(i, j)] = 1
+        orig, board[i][j] = board[i][j], '.'
         ans = (
-            self.helper(board, i - 1, j, covered, word[1:]) or
-            self.helper(board, i + 1, j, covered, word[1:]) or
-            self.helper(board, i, j - 1, covered, word[1:]) or
-            self.helper(board, i, j + 1, covered, word[1:])
+            self.helper(board, i - 1, j, word[1:]) or
+            self.helper(board, i + 1, j, word[1:]) or
+            self.helper(board, i, j - 1, word[1:]) or
+            self.helper(board, i, j + 1, word[1:])
         )
-        del covered[(i, j)]
+        board[i][j] = orig
         
         return ans
-
-# A B C E
-# S F E S
-# A D E E

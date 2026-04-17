5680f38e0043afe9f98505210fd83231d6a019af
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-04-18 03:26:58 +0530

    Time: 3707 ms (59.06%), Space: 19.2 MB (95.69%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -2,26 +2,25 @@ class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
         for i in range(len(board)):
             for j in range(len(board[0])):
-                if self.helper(board, i, j, word):
+                if self.helper(board, i, j, word, 0):
                     return True
         return False
 
-    def helper(self, board, i, j, word):
-        if not word:
+    def helper(self, board, i, j, word, ind):
+        if ind == len(word):
             return True
         elif (
             not (0 <= i < len(board) and 0 <= j < len(board[0])) or 
-            board[i][j] == '.' or 
-            board[i][j] != word[0]
+            board[i][j] != word[ind]
         ):
             return False
 
         orig, board[i][j] = board[i][j], '.'
         ans = (
-            self.helper(board, i - 1, j, word[1:]) or
-            self.helper(board, i + 1, j, word[1:]) or
-            self.helper(board, i, j - 1, word[1:]) or
-            self.helper(board, i, j + 1, word[1:])
+            self.helper(board, i - 1, j, word, ind + 1) or
+            self.helper(board, i + 1, j, word, ind + 1) or
+            self.helper(board, i, j - 1, word, ind + 1) or
+            self.helper(board, i, j + 1, word, ind + 1)
         )
         board[i][j] = orig
         

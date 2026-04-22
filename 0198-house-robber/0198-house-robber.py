58c20ae7726484718db3ba79c37c61197836ca6a
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-04-23 01:40:44 +0530

    Time: 0 ms (100%), Space: 19.1 MB (98.73%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -1,20 +1,18 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        if len(nums) == 1:
-            return nums[0]
-        
-        dp = [0 for i in range(len(nums))]
-        dp[0], dp[1] = nums[0], max(nums[0], nums[1])
+        v1 = v2 = 0 # v1 = dp[i-2], v2 = dp[i-1] for current i
 
-        for i in range(2, len(nums)):
-            dp[i] = max(dp[i - 2] + nums[i], dp[i - 1])
+        for num in nums:
+            curr = max(num + v1, v2)
+            v1 = v2
+            v2 = curr
         
-        return dp[-1]
+        return v2
 
 '''
 Approach: DP
 - Define dp[i] = max money you can rob up to index i.
 - Transition: dp[i] = max(dp[i-1], nums[i] + dp[i-2]) (skip vs take).
 - Base cases: dp[0] = nums[0], dp[1] = max(nums[0], nums[1]).
-- Optimize space by keeping only two variables (prev1, prev2) instead of full DP array.
+- Optimize space by keeping only two variables (v1, v2) instead of full DP array.
 '''

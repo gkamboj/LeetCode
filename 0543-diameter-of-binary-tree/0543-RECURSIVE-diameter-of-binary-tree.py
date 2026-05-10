3d968d915a0fa6556838541a0096051fc211779b
Gaurav Kamboj <gauravkambojtu@gmail.com>
2026-05-10 16:08:21 +0530

    Time: 3 ms (75.46%), Space: 22.1 MB (64.39%)

--- 0543-diameter-of-binary-tree/0543-RECURSIVE-diameter-of-binary-tree.py
+++ 0543-diameter-of-binary-tree/0543-RECURSIVE-diameter-of-binary-tree.py
@@ -8,19 +8,19 @@ class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
         self.ans = 0
         
-        def dfs(root):
-            if not root:
+        def dfs(node): # returns height of subtree rooted at node (distance from node to deepest leaf node)
+            if not node:
                 return 0
-            left = dfs(root.left) + 1 # depth of left tree including root (added 1 for root)
-            right = dfs(root.right) + 1 # defpth of right tree including root (added 1 for root)
-            self.ans = max(self.ans, left + right - 2) # subtracted 2 as diameter will be 2 less than sum of both depths
-            return max(left, right)
+            left = dfs(node.left) # height of left tree
+            right = dfs(node.right) # height of right tree
+            self.ans = max(self.ans, left + right) # longest path passing through current node - left + right
+            return 1 + max(left, right) # height = 1 + max of left and right subtree heights
         
         dfs(root)
         return self.ans
-    
+
 '''
-Approach: Find depth of left as well as right part of tree (including root) recursively and take maximum of
-those less 2 for diameter. Refer in-line comments and NC video (https://www.youtube.com/watch?v=bkxqA8Rfv04) 
-for detail.
+Approach:
+Find the height of the left as well as the right part of the tree recursively and take the maximum of those. Refer in-line comments and 
+NC video (https://www.youtube.com/watch?v=bkxqA8Rfv04) for details.
 '''
